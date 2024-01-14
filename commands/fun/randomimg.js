db80e4f339eedcf479ef1aaf82c3b5843228b296
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-01-15 00:15:09 +0100

    brought back and fixed randompic.js

--- commands/fun/randomimg.js
+++ commands/fun/randomimg.js
@@ -8,13 +8,16 @@ module.exports = {
         .setName("randompic")
         .setDescription("Send a random image using the picsum.photos api."),
     async execute(interaction) {
+        // There are currently 1084 photos on picsum.images
+        // And we need an ID to keep a requested image
+        const randomImageId = (Math.floor(Math.random() * 1085));
 
         const embedReply = new EmbedBuilder({
             color: 0x5F0FD6,
             title: "Random pic.",
-            description: "Here is a random 512x512 image from picsum.photos:",
+            description: "Here is a random 512x512 image from picsum.photos (might take some time to load):",
             image: {
-                url: "https://picsum.photos/512/512"
+                url: `https://picsum.photos/id/${randomImageId}/512/512`
             },
             timestamp: new Date().toISOString(),
             footer: {
