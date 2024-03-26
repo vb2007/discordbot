827c463d0ac8f87fdc4abfd284d753daa83197d4
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-03-26 21:53:15 +0100

    made scraping, caching to json, and choosing randomly (down bad)

--- commands/fun/randomfeet.js
+++ commands/fun/randomfeet.js
@@ -1,5 +1,44 @@
 const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
 const fs = require("fs");
+const path = require("path");
+const linksFile = './data/links.json'; 
+const request = require('request-promise');
+const cheerio = require('cheerio');
+
+async function scrapeLinks(url) {
+    const res = await request(url);
+    const $ = cheerio.load(res);
+
+    const validExts = [".webp", ".jpg", ".jpeg", ".png"];
+    const links = [];
+
+    $('a').each((i, link) => {
+        let href = (url + $(link).attr('href'));
+
+        const ext = path.extname(href);
+
+        if(validExts.includes(ext)) {
+            links.push(href);
+        }
+    });
+    
+    return links;
+}
+
+async function loadLinks() {
+    let links = [];
+    
+    try {
+        links = JSON.parse(fs.readFileSync(linksFile));
+    }
+    catch {
+        links = await scrapeLinks("https://vb2007.hu/extended-cdn/feetpics/");
+
+        // await fs.writeFileSync(linksFile, JSON.stringify(links));
+    }
+
+    return links;
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,27 +48,31 @@ module.exports = {
         //vár ha egyszerre túl sok a lábkép kérelem...
         await interaction.deferReply();
 
-        const labkepek = [
-            "https://vb2007.hu/extended-cdn/feetpics/bsmaci1.jpg",
-            "https://vb2007.hu/extended-cdn/feetpics/forgacs.webp",
-            "https://vb2007.hu/extended-cdn/feetpics/py1.png",
-            "https://vb2007.hu/extended-cdn/feetpics/py2.webp",
-            "https://vb2007.hu/extended-cdn/feetpics/py3.webp",
-            "https://vb2007.hu/extended-cdn/feetpics/py4.webp",
-            "https://vb2007.hu/extended-cdn/feetpics/py5.webp",
-            "https://vb2007.hu/extended-cdn/feetpics/skelly1.jpg",
-            "https://vb2007.hu/extended-cdn/feetpics/skelly2.jpg",
-            "https://vb2007.hu/extended-cdn/feetpics/skelly3.jpg",
-            "https://vb2007.hu/extended-cdn/feetpics/skelly4.jpg",
-            "https://vb2007.hu/extended-cdn/feetpics/theevilapple1.jpg",
-            "https://vb2007.hu/extended-cdn/feetpics/theevilapple2.jpg",
-            "https://vb2007.hu/extended-cdn/feetpics/theevilapple3.jpg",
-            "https://vb2007.hu/extended-cdn/feetpics/theevilapple4.jpg",
+        // const feetpics = [
+        //     "https://vb2007.hu/extended-cdn/feetpics/special/bsmaci1.jpg",
+        //     "https://vb2007.hu/extended-cdn/feetpics/special/forgacs.webp",
+        //     "https://vb2007.hu/extended-cdn/feetpics/special/py1.png",
+        //     "https://vb2007.hu/extended-cdn/feetpics/special/py2.webp",
+        //     "https://vb2007.hu/extended-cdn/feetpics/special/py3.webp",
+        //     "https://vb2007.hu/extended-cdn/feetpics/special/py4.webp",
+        //     "https://vb2007.hu/extended-cdn/feetpics/special/py5.webp",
+        //     "https://vb2007.hu/extended-cdn/feetpics/special/skelly1.jpg",
+        //     "https://vb2007.hu/extended-cdn/feetpics/special/skelly2.jpg",
+        //     "https://vb2007.hu/extended-cdn/feetpics/special/skelly3.jpg",
+        //     "https://vb2007.hu/extended-cdn/feetpics/special/skelly4.jpg",
+        //     "https://vb2007.hu/extended-cdn/feetpics/special/tea1.jpg",
+        //     "https://vb2007.hu/extended-cdn/feetpics/special/tea2.jpg",
+        //     "https://vb2007.hu/extended-cdn/feetpics/special/tea3.jpg",
+        //     "https://vb2007.hu/extended-cdn/feetpics/special/tea4.jpg",
+        //     "https://vb2007.hu/extended-cdn/feetpics/special/umberto.webp",
+        // ]
 
-        ]
+        const links = await loadLinks();
 
         //random képet választ a listából
-        const randomFeet = labkepek[Math.floor(Math.random() * labkepek.length)];
+        const randomFeet = links[Math.floor(Math.random() * links.length)];
+
+        console.log(randomFeet);
 
         const embedReply = new EmbedBuilder({
             color: 0x5F0FD6,
