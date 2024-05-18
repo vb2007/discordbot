b17663b5ed227d02fbf3d53c6e961a907bd4a4ea
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-05-18 15:50:03 +0000

    (hopefully) finished autorole disable

--- commands/administration/autorole-disable.js
+++ commands/administration/autorole-disable.js
@@ -7,30 +7,38 @@ module.exports = {
         .setDescription("Disables the role that is automatically assigned to new members on join.")
         .setDefaultMemberPermissions(PermissionFlagBits.Administartor),
     async execute(interaction) {
-        try {
-            let currentGuildId = interaction.guild.id;
-            let autoroleGuildId = await db.query("SELECT guildId FROM autorole WHERE guildId = ?", [currentGuildId]);
-            if (autoroleGuildId == currentGuildId) {
-
-            }
-            else {
-
-            }
-
-            var embedReply = new EmbedBuilder({
-                color: 0x5F0FD6,
-                title: "Disabling autorole.",
-                description: replyContent,
-                timestamp: new Date().toISOString(),
-                footer: {
-                    text: `Requested by: ${interaction.user.username}` ,
-                    icon_url: interaction.user.displayAvatarURL({ dynamic: true }),
-                },
-            });
-
-            await interaction.reply({ embeds: [embedReply] });
-        } catch (error) {
-            console.log(error);
+        if (!interaction.inGuild()) {
+            var replyContent = "You can only disable autorole in a server."
         }
-    }
-}
+        else {
+            try {
+                let currentGuildId = interaction.guild.id;
+                let autoroleGuildId = await db.query("SELECT guildId FROM autorole WHERE guildId = ?", [currentGuildId]);
+
+                if (autoroleGuildId == currentGuildId) {
+                    await db.query("DELETE FROM autorole WHERE guildId = ?", [autoroleGuildId]);
+
+                    var replyContent = "The autorole feature has been disabled succesfully.\nYou can re-enable it with `/autorole-configure`."
+                }
+                else {
+                    var replyContent = "Autorole has not been configured for this server.\nTherefore, you can't disable it."
+                }
+
+                var embedReply = new EmbedBuilder({
+                    color: 0x5F0FD6,
+                    title: "Disabling autorole.",
+                    description: replyContent,
+                    timestamp: new Date().toISOString(),
+                    footer: {
+                        text: `Requested by: ${interaction.user.username}` ,
+                        icon_url: interaction.user.displayAvatarURL({ dynamic: true }),
+                    },
+                });
+
+                await interaction.reply({ embeds: [embedReply] });
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    },
+};
