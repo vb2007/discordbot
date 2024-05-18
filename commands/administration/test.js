c7d7e86632d5e1dad91bd969ade9ab8ecb1cad8b
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-05-18 15:51:29 +0000

    removed test file

--- commands/administration/test.js
+++
@@ -1,7 +0,0 @@
-// const { EmbedBuilder, SlashCommandBuilder, PermissionFlagBits } = require("discord.js");
-const db = require("../../db");
-
-guildId = "1";
-roleId = "1";
-
-db.query("INSERT INTO autorole (guildId, roleId) VALUES (?, ?) ON DUPLICATE KEY UPDATE roleId = ?", [guildId, roleId, roleId] );
