eca9a38ccfa61939ff1877408c7c5522806cbead
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-06-30 18:16:04 +0200

    added await before queries, fixed queries, disabled command in DMs, updated thirtyMinutesAgo to work with UTC time, made checks to target user's balance

--- commands/economy/rob.js
+++ commands/economy/rob.js
@@ -10,31 +10,55 @@ module.exports = {
             option
                 .setName("target")
                 .setDescription("Choose the target you want to rob from.")
-                .setRequired(true)),
+                .setRequired(true))
+        .setDMPermission(false),
     async execute(interaction) {
         if (!interaction.inGuild()) {
             var replyContent = "You can only rob from a member in a server.";
         }
         else {
-            interactonUserId = interaction.user.id;
-            const query = await db.query("SELECT lastRobTime FROM economy WHERE userId = ?", [interactionUserId]);
-            const lastTimeRobbed = query[0]?.lastTimeRobbed || null;
+            const query = await db.query("SELECT lastRobTime FROM economy WHERE userId = ?", [interaction.user.id]);
+            const lastRobTime = query[0]?.lastRobTime || null;
 
-            const thirtyMinutesAgo = new Date(Date.now() - 30 * 60 * 1000);
+            const thirtyMinutesAgoUTC = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60000 - 30 * 60000);
 
-            if (lastTimeRobbed > thirtyMinutesAgo || !lastTimeRobbed) {
-                const targetUserName = interaction.options.getUser("target").tag;
+            if (!lastRobTime || lastRobTime <= thirtyMinutesAgoUTC) {
+                console.log(lastRobTime);
+                console.log(thirtyMinutesAgoUTC);
                 const targetUserId = interaction.options.getUser("target").id;
-                const robAmount = Math.floor(Math.random() * 10000);
 
-                //adds the robbet amount to the interaction's user
-                db.query("UPDATE economy SET money = money + ?, lastRobTime = ? WHERE userId = ?", [robAmount, Date.now(), targetUserId]);
-                //removes the robbet amount from the target user
-                db.query("UPDATE economy SET money = money - ? WHERE userId = ?", [robAmount, targetUserId]);
-                var replyContent = `Successfully robbed **${robAmount}** from **${targetUserName}**.`;
+                const query = await db.query("SELECT balance FROM economy WHERE userId = ?", [targetUserId]);
+                const targetUserBalance = query[0]?.balance || null;
+
+                if (targetUserBalance > 50) {
+                    const targetUserName = interaction.options.getUser("target").tag;
+                    const robAmount = Math.floor(Math.random() * 50);
+
+                    //removes the robbed amount from the target user
+                    await db.query("UPDATE economy SET balance = balance - ? WHERE userId = ?",
+                        [
+                            robAmount,
+                            targetUserId
+                        ]
+                    );
+
+                    //adds the robbed amount to the interaction's executor
+                    await db.query("UPDATE economy SET balance = balance + ?, lastRobTime = ? WHERE userId = ?",
+                        [
+                            robAmount,
+                            new Date().toISOString().slice(0, 19).replace('T', ' '),
+                            interaction.user.id
+                        ]
+                    );
+
+                    var replyContent = `Successfully robbed $**${robAmount}** from **${targetUserName}**.`;
+                }
+                else {
+                    var replyContent = `The user you're trying to rob from must have a minimum of **$50**.\nPlease choose another target.\n**TIP:** You can check how much money other memebers have with the \`/leaderboard\` command.`;
+                }
             }
             else {
-                var replyContent = `You've already robbed in the last 30 minutes.\nPlease wait another ${Math.ceil((lastTimeRobbed.getTime() - thirtyMinutesAgo.getTime()) / 60000)} minutes before trying to rob again.`;
+                var replyContent = `You've already robbed in the last 30 minutes.\nPlease wait another ${Math.ceil((lastRobTime.getTime() - thirtyMinutesAgoUTC.getTime()) / 60000)} minutes before trying to rob again.`;
             }
         }
 
