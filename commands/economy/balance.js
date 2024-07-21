2f006fddca588e2023d574558505e7a30f2f6592
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-07-21 22:26:48 +0200

    added /balance command (partially, needs fixing)

---
+++ commands/economy/balance.js
@@ -0,0 +1,50 @@
+const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+const db = require("../../db");
+const { logToFileAndDatabase } = require("../../logger");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("balance")
+        .setDescription("Displays a user's balance.")
+        .addUserOption(option =>
+            option
+                .setName("user")
+                .setDescription("Choose who's balance you'd like to see.")
+                .setRequired(false))
+        .setDMPermission(false),
+    async execute(interaction) {
+        if(!interaction.inGuild()) {
+            var replyContent = "You can only check a member's balance in a server.";
+        }
+        else {
+            const userId = interaction.user.id;
+            const targetUserId = interaction.options.getUser("user")?.id || null;
+
+            if(!targetUserId) {
+                var query = await db.query("SELECT balance FROM economy WHERE userId = ?", [userId]);
+            }
+            else {
+                var query = await db.query("SELECT balance FROM economy WHERE userId = ?", [targetUserId]);
+            }
+
+            var replyContent = `${userId}'s balance is **${query[0]?.balance}**. :moneybag:`;
+        }
+
+        var embedReply = new EmbedBuilder({
+            color: 0x5F0FD6,
+            title: "Checking user balance.",
+            description: replyContent,
+            timestamp: new Date().toISOString(),
+            footer: {
+                text: `Requested by: ${interaction.user.username}`,
+                icon_url: interaction.user.displayAvatarURL({ dynamic: true })
+            }
+        });
+
+        await interaction.reply({ embeds: [embedReply] });
+
+        //logging
+        const response = JSON.stringify(embedReply.toJSON());
+        await logToFileAndDatabase(interaction, response);
+    }
+}
