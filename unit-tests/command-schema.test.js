fbd7a943dc6294eaf1135f9aa9023e366bc91393
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-07-29 20:50:17 +0200

    initial: added command schema checking unit test

---
+++ unit-tests/command-schema.test.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+describe("Command structure test", () => {
+    test("All commands should have \"data\" and \"execute\" properties", () => {
+        const foldersPath = path.join(__dirname, "..", "commands");
+        const commandFolders = fs.readdirSync(foldersPath);
+
+        commandFolders.forEach(folder => {
+            const commandsPath = path.join(foldersPath, folder);
+            const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+            commandFiles.forEach(file => {
+                const filePath = path.join(commandsPath, file);
+                const command = require(filePath);
+
+                expect(command).toHaveProperty("data");
+            })
+        })
+    });
+});
