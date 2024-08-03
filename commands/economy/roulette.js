ef2167afceb9b563d8c48ecdfed24175dd3f1529
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-08-03 19:19:45 +0200

    added first code parts for the /leaderboard command

---
+++ commands/economy/roulette.js
@@ -0,0 +1,95 @@
+const { EmbedBuilder, SlashCommandBuilder, embedLength, Embed } = require("discord.js");
+const logToFileAndDatabase = require("../../logger");
+const db = require("../../db");
+const { execute } = require("./leaderboard");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("roulette")
+        .setDescription("Try your luck and play a game of roulette.")
+        .addIntegerOption(option =>
+            option
+                .setName("amount")
+                .setDescription("The amount of money you would like to play with.")
+                .setRequired(true))
+        .addStringOption(option =>
+            option
+                .setName("color")
+                .setDescription("The color you would like to to bet on.")
+                .addChoices(
+                    { name: "Red", value: "red" },
+                    { name: "Black", value: "black" },
+                    { name: "Green", value: "green" }
+                )
+                .setRequired(true)
+        )
+        .setDMPermission(false),
+    async execute(interaction) {
+        if (!interaction.inGuild()) {
+            var replyContent = "You can only play roulette in a server.";
+        }
+        else {
+            const rouletteNumbers = Array.from({ length: 37}, (_, index) => index + 1); //37 = european roulette. this might be configureable later.
+
+            function generateRandomOutcome() {
+                const randomIndex = Math.floor(Math.random() * rouletteNumbers.length);
+                const number = rouletteNumbers[randomIndex]
+                //green if number is 0, black if it's devideable by 2, red othervise 
+                const color = number === 0 ? "green" : (number % 2 === 0 ? "balck" : "red");
+                return { number, color };
+            }
+
+            var interactionUserId = interaction.user.id;
+
+            var amount = interaction.option.getInteger("amount");
+            var guessedColor = interaction.option.getString("color");
+
+            var randomOutcome = generateRandomOutcome();
+
+            var randomColor = randomOutcome.color;
+            var randomNumber = randomOutcome.number;
+
+            switch (guessedColor) {
+                case randomColor:
+                    await db.query("UPDATE economy SET balance = balance + ? WHERE userId = ?",
+                        [
+                            amount * 2,
+                            interactionUserId
+                        ]
+                    );
+
+                    var replyContent = `The ball landed on ${randomColor} ${randomNumber}.\nYour guess was ${randomColor} as well! :money_mouth:`;
+                    break;
+                case !randomColor:
+                    await db.query("UPDATE economy SET balance = balance - ? WHERE userId = ?",
+                        [
+                            amount,
+                            interactionUserId
+                        ]
+                    );
+
+                    var replyContent = `The ball landed on ${randomColor} ${randomNumber}.\nYour guess was ${randomColor}.\nMaybe try your luck again. :upside_down:`;
+                    break
+                default:
+                    var replyContent = "The color you've chosen is invalid.\nPlease choose from *red*, *black* or *green*.";
+            }
+
+            var embedReply = new EmbedBuilder({
+                color: 0x5F0FD6,
+                title: "Playing roulette.",
+                description: replyContent,
+                timestamp: new Date().toISOString(),
+                footer: {
+                    text: `Requested by: ${interaction.user.username}`,
+                    icon_url: interaction.user.displayAvatarUrl({ dynamic: true })
+                }
+            });
+
+            await interaction.reply({ embeds: [embedReply] })
+
+            //logging
+            const response = JSON.stringify(embedReply.toJSON());
+            await logToFileAndDatabase(interaction, response);
+        }
+    }
+}
