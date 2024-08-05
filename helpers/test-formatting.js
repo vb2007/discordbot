d5ed8c0071b221da6c49479df1243bd173c32bec
vb2007 <admin@vb2007.hu>
2024-08-05 12:17:02 +0200

    added first helper for uppercasing a string's first letter

---
+++ helpers/test-formatting.js
@@ -0,0 +1,4 @@
+function capitalizeFirstLetter(string) {
+    var stringWithCapitalizedFirstLetter = string.charAt(0).toUpperCase() + string.slice(1);
+    return stringWithCapitalizedFirstLetter;
+}
