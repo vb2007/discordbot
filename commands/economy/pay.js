52b782006508e29230050eda52df9d74f5b5a426
vb2007 <admin@vb2007.hu>
2024-08-07 13:23:09 +0200

    updated / pay

--- commands/economy/pay.js
+++ commands/economy/pay.js
@@ -18,11 +18,35 @@ module.exports = {
                 .setRequired(true))
         .setDMPermission(false),
     async execute(interaction) {
+        if (!interaction.inGuild) {
+            var replyContent = "You can only pay out a member in a server.";
+        }
+        else {
+            const amount = interaction.options.getInteger("amount");
+            const targetUser = interaction.option.getUser("target");
+            const interactionUserId = interaction.user.id;
+
+            const query = await db.query("SELECT balance FROM economy WHERE userId = ?", []);
+            const userBalance = query[0]?.balance || null;
+
+            if (amount < userBalance) {
+                var replyContent = `You can't pay that much money to ${targetUser}!\nYour balance is only \`$${userBalance}\`.`;
+            }
+        }
 
         const embedReply = new EmbedBuilder({
-
+            color: 0x5F0FD6,
+            title: "Paying to a member.",
+            description: replyContent,
+            timestamp: new Date().toISOString(),
+            footer: {
+                text: `Requested by: ${interaction.user.username}` ,
+                icon_url: interaction.user.displayAvatarURL({ dynamic: true }),
+            },
         });
 
-        interaction.reply();
+        interaction.reply({ embeds: embedReply});
+
+        
     }
 }
