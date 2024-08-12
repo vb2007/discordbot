6e142e2ee0546d45f5aea11221f90ba0cd4857b9
vb2007 <admin@vb2007.hu>
2024-08-12 14:17:29 +0200

    added config schema validation

---
+++ scripts/verify-config-schema.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+const Ajv = require("ajv");
+
+const configPath = path.join(__dirname, "..", "config.json");
+const schemaPath = path.join(__dirname, "config-schema.json");
+
+const config = JSON.parse(fs.readFileSync(configPath, "utf-8"));
+const configSchema = JSON.parse(fs.readFileSync(configSchemaPath, "utf-8"));
+
+const ajv = new Ajv();
+const validate = ajv.compile(configSchema);
+const valid = validate(config);
+
+if (!valid) {
+    console.error($`The config.json file contains syntax errors: ${validate.errors}\nPlease follow the schema that's present on the project's GitHub page: https://github.com/vb2007/discordbot/tree/dev-bank?tab=readme-ov-file#setting-up-the-bot`);
+    process.exit(1);
+}
