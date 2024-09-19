9fa5250212c2973365d21b4729543ed5d2a19c41
vb2007 <admin@vb2007.hu>
2024-09-19 12:27:13 +0200

    used the embed helper function in the /ping-db command

--- commands/utility/ping-db.js
+++ commands/utility/ping-db.js
@@ -1,4 +1,5 @@
-const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
+const { embedReplyPrimaryColorWithFields, embedReplyFailureColor } = require("../../helpers/embed-reply");
 const db = require("../../helpers/db");
 const { logToFileAndDatabase } = require("../../helpers/logger");
 
@@ -7,10 +8,7 @@ module.exports = {
         .setName("ping-db")
         .setDescription("Displays the the bot's MariaDB database's current latency."),
     async execute(interaction) {
-        // let conn;
         try {
-            // let conn;
-            // conn = await db.pool.getConnection();
 
             const startTime = Date.now();
             await db.query("SELECT 1");
@@ -18,26 +16,25 @@ module.exports = {
 
             const latency = endTime - startTime;
 
-            var embedRespone = `**${latency}ms** or **${latency / 1000}s**`;
+            const embedReply = embedReplyPrimaryColorWithFields(
+                "Ping the database",
+                "",
+                [
+                    { name: "Pong from the database! :ping_pong:", value: "" },
+                    { name: "Response time: ", value: `**${latency}ms** or **${latency / 1000}s**` },
+                ],
+                interaction
+            );
         }
         catch (error) {
             console.error(error);
-            var embedRespone = "**Error:** Database connection failed.\nIf this issue persists, please [report it on GitHub](https://github.com/vb2007/discordbot/issues/new).";
-        }
 
-        const embedReply = new EmbedBuilder({
-            color: 0x5F0FD6,
-            title: "Ping the database.",
-            fields: [
-                { name: "Pong from the database! :ping_pong:", value: "" },
-                { name: "Response time: ", value: embedRespone, inline: true },
-            ],
-            timestamp: new Date().toISOString(),
-            footer: {
-                text: `Requested by: ${interaction.user.username}`,
-                icon_url: interaction.user.displayAvatarURL({ dynamic: true })
-            }
-        });
+            const embedReply = embedReplyFailureColor(
+                "Ping the database - Error",
+                "**Error:** Database connection failed.\nIf this issue persists, please [report it on GitHub](https://github.com/vb2007/discordbot/issues/new).",
+                interaction
+            );
+        }
 
         await interaction.reply({ embeds: [embedReply] });
 
