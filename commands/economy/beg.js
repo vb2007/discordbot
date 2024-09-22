442a03eadc012c816ffdde905111591544a3170d
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-09-23 01:22:57 +0200

    added outcome chances to /beg

--- commands/economy/beg.js
+++ commands/economy/beg.js
@@ -14,52 +14,65 @@ module.exports = {
         const lastBegTime = query[0]?.lastBegTime || null;
         const nextApprovedBegTimeUTC = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60000 - 10 * 60000); //10 minutes
 
-        const amount = Math.floor(Math.random() * 85);
-        if (userId) {
-            if (!lastBegTime || lastBegTime <= nextApprovedBegTimeUTC) {
-                await db.query("UPDATE economy SET balance = balance + ?, lastBegTime = ? WHERE userId = ?",
+        const outcomeChance = Math.floor(Math.random() * 100);
+
+        //60% chance for getting some money
+        if (outcomeChance < 60) {
+            const amount = Math.floor(Math.random() * 85);
+            if (userId) {
+                if (!lastBegTime || lastBegTime <= nextApprovedBegTimeUTC) {
+                    await db.query("UPDATE economy SET balance = balance + ?, lastBegTime = ? WHERE userId = ?",
+                        [
+                            amount,
+                            new Date().toISOString().slice(0, 19).replace('T', ' '),
+                            userId
+                        ]
+                    );
+    
+                    var embedReply = embedReplySuccessColor(
+                        "Begging.",
+                        `You've begged and some random guy gave you \`$${amount}\` dollars.`,
+                        interaction
+                    );
+                }
+                else {
+                    const remainingTimeInSeconds = Math.ceil((lastBegTime.getTime() - nextApprovedBegTimeUTC.getTime()) / 1000);
+                    const remainingMinutes = Math.floor(remainingTimeInSeconds / 60);
+                    const remainingSeconds = remainingTimeInSeconds % 60;
+    
+                    var embedReply = embedReplyFailureColor(
+                        "Beg - Error",
+                        `You've already begged in the last 10 minutes.\nPlease wait **${remainingMinutes} minute(s)** and **${remainingSeconds} second(s)** before trying to **beg** again.`,
+                        interaction
+                    );
+                }
+            }
+            else {
+                //if it's the executor's first time using any economy command (so it's userId is not in the database yet...)
+                await db.query("INSERT INTO economy (userName, userId, balance, firstTransactionDate, lastBegTime) VALUES (?, ?, ?, ?, ?)",
                     [
+                        interaction.user.username,
+                        interaction.user.id,
                         amount,
                         new Date().toISOString().slice(0, 19).replace('T', ' '),
-                        userId
+                        new Date().toISOString().slice(0, 19).replace('T', ' ')
                     ]
                 );
-
-                var embedReply = embedReplySuccessColor(
+    
+                var embedReply = embedSuccessColor(
                     "Begging.",
                     `You've begged and some random guy gave you \`$${amount}\` dollars.`,
                     interaction
                 );
             }
-            else {
-                const remainingTimeInSeconds = Math.ceil((lastBegTime.getTime() - nextApprovedBegTimeUTC.getTime()) / 1000);
-                const remainingMinutes = Math.floor(remainingTimeInSeconds / 60);
-                const remainingSeconds = remainingTimeInSeconds % 60;
-
-                var embedReply = embedReplyFailureColor(
-                    "Beg - Error",
-                    `You've already begged in the last 10 minutes.\nPlease wait **${remainingMinutes} minute(s)** and **${remainingSeconds} second(s)** before trying to **beg** again.`,
-                    interaction
-                );
-            }
         }
-        else {
-            //if it's the executor's first time using any economy command (so it's userId is not in the database yet...)
-            await db.query("INSERT INTO economy (userName, userId, balance, firstTransactionDate, lastBegTime) VALUES (?, ?, ?, ?, ?)",
-                [
-                    interaction.user.username,
-                    interaction.user.id,
-                    amount,
-                    new Date().toISOString().slice(0, 19).replace('T', ' '),
-                    new Date().toISOString().slice(0, 19).replace('T', ' ')
-                ]
-            );
+        //30% chance for getting nothing
+        else if (outcomeChance < 90) {
 
-            var embedReply = embedSuccessColor(
-                "Begging.",
-                `You've begged and some random guy gave you \`$${amount}\` dollars.`,
-                interaction
-            );
+        }
+        //10% chance for loosing money
+        else {
+
         }
 
         await interaction.reply({ embeds: [embedReply] });
