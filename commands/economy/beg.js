d58c14ce4d4485341e6d868dd827ea13780d4ab0
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-09-23 01:56:47 +0200

    fixed cases in /beg

--- commands/economy/beg.js
+++ commands/economy/beg.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js");
-const { embedReplySuccessColor, embedReplyFailureColor } = require("../../helpers/embed-reply");
+const { embedReplySuccessColor, embedReplyWarningColor, embedReplyFailureColor } = require("../../helpers/embed-reply");
 const { logToFileAndDatabase } = require("../../helpers/logger");
 const db = require("../../helpers/db");
 
@@ -15,12 +15,12 @@ module.exports = {
         const nextApprovedBegTimeUTC = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60000 - 10 * 60000); //10 minutes
 
         const outcomeChance = Math.floor(Math.random() * 100);
+        const amount = Math.floor(Math.random() * 85);
 
-        //60% chance for getting some money
-        if (outcomeChance < 60) {
-            const amount = Math.floor(Math.random() * 85);
-            if (userId) {
-                if (!lastBegTime || lastBegTime <= nextApprovedBegTimeUTC) {
+        if (userId) {
+            if (!lastBegTime || lastBegTime <= nextApprovedBegTimeUTC) {
+                //60% chance for getting some money
+                if (outcomeChance < 60) {        
                     await db.query("UPDATE economy SET balance = balance + ?, lastBegTime = ? WHERE userId = ?",
                         [
                             amount,
@@ -28,51 +28,67 @@ module.exports = {
                             userId
                         ]
                     );
-    
+
                     var embedReply = embedReplySuccessColor(
                         "Begging.",
                         `You've begged and some random guy gave you \`$${amount}\` dollars.`,
                         interaction
                     );
                 }
+                //30% chance for getting nothing
+                else if (outcomeChance < 90) {
+                    var embedReply = embedReplyWarningColor(
+                        "Begging.",
+                        `While you were begging on the street, a random guy just kicked you in the balls and left you alone with nothing.`,
+                        interaction
+                    );
+                }
+                //10% chance for loosing money
                 else {
-                    const remainingTimeInSeconds = Math.ceil((lastBegTime.getTime() - nextApprovedBegTimeUTC.getTime()) / 1000);
-                    const remainingMinutes = Math.floor(remainingTimeInSeconds / 60);
-                    const remainingSeconds = remainingTimeInSeconds % 60;
-    
+                    await db.query("UPDATE economy SET balance = balance + ?, lastBegTime = ? WHERE userId = ?",
+                        [
+                            amount,
+                            new Date().toISOString().slice(0, 19).replace('T', ' '),
+                            userId
+                        ]
+                    );
+
                     var embedReply = embedReplyFailureColor(
-                        "Beg - Error",
-                        `You've already begged in the last 10 minutes.\nPlease wait **${remainingMinutes} minute(s)** and **${remainingSeconds} second(s)** before trying to **beg** again.`,
+                        "Begging.",
+                        `While you were begging near a trash can, a random guy (with a dark skin tone) took the coins from you cup, then ran away.\nYou've lost \`$${amount}\` dollars.`,
                         interaction
                     );
                 }
             }
             else {
-                //if it's the executor's first time using any economy command (so it's userId is not in the database yet...)
-                await db.query("INSERT INTO economy (userName, userId, balance, firstTransactionDate, lastBegTime) VALUES (?, ?, ?, ?, ?)",
-                    [
-                        interaction.user.username,
-                        interaction.user.id,
-                        amount,
-                        new Date().toISOString().slice(0, 19).replace('T', ' '),
-                        new Date().toISOString().slice(0, 19).replace('T', ' ')
-                    ]
-                );
-    
-                var embedReply = embedSuccessColor(
-                    "Begging.",
-                    `You've begged and some random guy gave you \`$${amount}\` dollars.`,
+                const remainingTimeInSeconds = Math.ceil((lastBegTime.getTime() - nextApprovedBegTimeUTC.getTime()) / 1000);
+                const remainingMinutes = Math.floor(remainingTimeInSeconds / 60);
+                const remainingSeconds = remainingTimeInSeconds % 60;
+
+                var embedReply = embedReplyFailureColor(
+                    "Begging - Error",
+                    `You've already begged in the last 10 minutes.\nPlease wait **${remainingMinutes} minute(s)** and **${remainingSeconds} second(s)** before trying to **beg** again.`,
                     interaction
                 );
             }
         }
-        //30% chance for getting nothing
-        else if (outcomeChance < 90) {
-
-        }
-        //10% chance for loosing money
         else {
+            //if it's the executor's first time using any economy command (so it's userId is not in the database yet...)
+            await db.query("INSERT INTO economy (userName, userId, balance, firstTransactionDate, lastBegTime) VALUES (?, ?, ?, ?, ?)",
+                [
+                    interaction.user.username,
+                    interaction.user.id,
+                    amount,
+                    new Date().toISOString().slice(0, 19).replace('T', ' '),
+                    new Date().toISOString().slice(0, 19).replace('T', ' ')
+                ]
+            );
 
+            var embedReply = embedSuccessColor(
+                "Begging.",
+                `You've begged and some random guy gave you \`$${amount}\` dollars.`,
+                interaction
+            );
         }
 
         await interaction.reply({ embeds: [embedReply] });
