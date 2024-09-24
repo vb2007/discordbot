c0f03ea6cea3fd3ecd0739ed0ede5472db9cac8a
Balázs Vágvölgyi <balazs.vagvolgyi@linistry.com>
2024-09-24 13:34:35 +0200

    added time calculation

--- commands/fun/911-countdown.js
+++ commands/fun/911-countdown.js
@@ -8,6 +8,19 @@ module.exports = {
         .setDescription("Displays how much time is left until 9/11.")
         .setDMPermission(true),
     async execute(interaction) {
-        
+        const now = new Date();
+        const currentYear = now.getUTCFullYear();
+        const targetDate = new Date(Date.UTC(currentYear, 8, 11)) //8, because months are 0-indexed
+
+        if (now > targetDate) {
+            targetDate.setUTCFullYear(currentYear + 1);
+        }
+
+        const timeDiffernce = targetDate - now;
+
+        const days = Math.floor(timeDiffernce / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDiffernce % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDiffernce % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDiffernce % (1000 * 60)) / 1000);
     }
 }
