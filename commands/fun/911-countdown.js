81a0e3faa9fd61a0c6345f6b7d1bd97b9a0061a4
Balázs Vágvölgyi <balazs.vagvolgyi@linistry.com>
2024-09-24 13:38:18 +0200

    made the command functional

--- commands/fun/911-countdown.js
+++ commands/fun/911-countdown.js
@@ -12,6 +12,7 @@ module.exports = {
         const currentYear = now.getUTCFullYear();
         const targetDate = new Date(Date.UTC(currentYear, 8, 11)) //8, because months are 0-indexed
 
+        //if 9/11 has already passed this year
         if (now > targetDate) {
             targetDate.setUTCFullYear(currentYear + 1);
         }
@@ -22,5 +23,17 @@ module.exports = {
         const hours = Math.floor((timeDiffernce % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((timeDiffernce % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeDiffernce % (1000 * 60)) / 1000);
+
+        const embedReply = embedReplyPrimaryColor(
+            "911 Countdown",
+            `Time left until 9/11: **${days} days ${hours} hours ${minutes} minutes ${seconds} seconds**.`,
+            interaction
+        );
+
+        await interaction.reply({ embeds: [embedReply] });
+
+        //logging
+        const response = JSON.stringify(embedReply.toJSON());
+		await logToFileAndDatabase(interaction, response);
     }
 }
