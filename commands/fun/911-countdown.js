397dd810d2b7c3c0420428665ce03d6381cf63c0
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-09-29 00:48:54 +0200

    updated /p11-countdown with a celebration message

--- commands/fun/911-countdown.js
+++ commands/fun/911-countdown.js
@@ -12,25 +12,33 @@ module.exports = {
         const currentYear = now.getUTCFullYear();
         const targetDate = new Date(Date.UTC(currentYear, 8, 11)) //8, because months are 0-indexed
 
-        //if 9/11 has already passed this year
-        if (now > targetDate) {
-            targetDate.setUTCFullYear(currentYear + 1);
+        if (now.getUTCMonth() === 8 && now.getUTCDate() === 11) {
+            var embedReply = embedReplyPrimaryColor(
+                "9/11 Countdown",
+                "Today is the day! :tada:\nHappy 9/11 everyone! :partying_face:",
+            );
+        }
+        else {
+            //if 9/11 has already passed this year
+            if (now > targetDate) {
+                targetDate.setUTCFullYear(currentYear + 1);
+            }
+
+            const timeDiffernce = targetDate - now;
+
+            const days = Math.floor(timeDiffernce / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeDiffernce % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeDiffernce % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeDiffernce % (1000 * 60)) / 1000);
+
+            var embedReply = embedReplyPrimaryColor(
+                "911 Countdown",
+                `Time left until 9/11: **${days} days ${hours} hours ${minutes} minutes ${seconds} seconds**.`,
+                interaction
+            );
         }
 
-        const timeDiffernce = targetDate - now;
-
-        const days = Math.floor(timeDiffernce / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeDiffernce % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeDiffernce % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeDiffernce % (1000 * 60)) / 1000);
-
-        const embedReply = embedReplyPrimaryColor(
-            "911 Countdown",
-            `Time left until 9/11: **${days} days ${hours} hours ${minutes} minutes ${seconds} seconds**.`,
-            interaction
-        );
-
-        await interaction.reply({ embeds: [embedReply] });
+        await interaction.reply({ embeds: [embedReply] });4
 
         //logging
         const response = JSON.stringify(embedReply.toJSON());
