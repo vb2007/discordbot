dc2528d68829559497671e7330a33f32a3ae5f91
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-09-28 22:28:01 +0200

    added "bank" leaderboard option to /leaderboard

--- commands/economy/leaderboard.js
+++ commands/economy/leaderboard.js
@@ -7,12 +7,20 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("leaderboard")
         .setDescription("Check the users with the most money on the server.")
+        .addStringOption(option =>
+            option
+                .setName("type")
+                .setDescription("The type of leaderboard (cash / bank) you want to see.")
+                .addChoices(
+                    { name: "Cash", value: "cash" },
+                    { name: "Bank", value: "bank" }
+                )
+                .setRequired(false))
         .addIntegerOption(option =>
             option
                 .setName("amount")
                 .setDescription("The amount of top users to display (MAX 100).")
-                .setRequired(false)
-        )
+                .setRequired(false))
         .setDMPermission(false),
     async execute(interaction) {
         var amount = interaction.options.getInteger("amount") || 10;
@@ -32,32 +40,72 @@ module.exports = {
             );
         }
         else {
-            var query = await db.query("SELECT userId, balance FROM economy ORDER BY balance DESC LIMIT ?", [amount]);
+            const type = interaction.options.getString("type") || "cash";
+            switch (type) {
+                case "cash":
+                    var query = await db.query("SELECT userId, balance FROM economy ORDER BY balance DESC LIMIT ?", [amount]);
 
-            var actualUserAmount = query.length;
+                    var actualUserAmount = query.length;
 
-            var replyContent = query.map((user, index) =>
-                `**${index + 1}**. <@${user.userId}> : \`$${user.balance}\` :moneybag:`
-            ).join('\n');
+                    var replyContent = query.map((user, index) =>
+                        `**${index + 1}**. <@${user.userId}> : \`$${user.balance}\` :moneybag:`
+                    ).join('\n');
 
-            var query = await db.query("SELECT COUNT(*) FROM economy");
-            var totalUserAmount = query[0]["COUNT(*)"];
+                    var query = await db.query("SELECT COUNT(*) FROM economy");
+                    var totalUserAmount = query[0]["COUNT(*)"];
 
-            var replyContent = replyContent + `\n\nEnd of the leaderboard. :eyes:\nThis server has **${totalUserAmount}** user${totalUserAmount !== 1 ? 's' : ''} with a balance.`;
+                    var replyContent = replyContent + `\n\nEnd of the leaderboard. :eyes:\nThis server has **${totalUserAmount}** user${totalUserAmount !== 1 ? 's' : ''} with a balance.`;
 
-            if (actualUserAmount == 0) {
-                var embedReply = embedReplyFailureColor(
-                    "Empty leaderboard.",
-                    "There are no users with a balance on this server.\nUse the `/work` command to be the first one on this list! :grin:",
-                    interaction
-                );
-            }
-            else {
-                var embedReply = embedReplyPrimaryColor(
-                    `Server leaderboard: TOP ${actualUserAmount} user${actualUserAmount !== 1 ? 's' : ''}.`,
-                    replyContent,
-                    interaction
-                );
+                    if (actualUserAmount == 0) {
+                        var embedReply = embedReplyFailureColor(
+                            "Empty leaderboard.",
+                            "There are no users with a balance on this server.\nUse the `/work` command to be the first one on this list! :grin:",
+                            interaction
+                        );
+                    }
+                    else {
+                        var embedReply = embedReplyPrimaryColor(
+                            `Server leaderboard: TOP ${actualUserAmount} user${actualUserAmount !== 1 ? 's' : ''}.`,
+                            replyContent,
+                            interaction
+                        );
+                    }
+                    break;
+                case "bank":
+                    var query = await db.query("SELECT userId, balanceInBank FROM economy ORDER BY balanceInBank DESC LIMIT ?", [amount]);
+
+                    var actualUserAmount = query.length;
+
+                    var replyContent = query.map((user, index) =>
+                        `**${index + 1}**. <@${user.userId}> : \`$${user.balanceInBank}\` :moneybag:`
+                    ).join('\n');
+
+                    var query = await db.query("SELECT COUNT(*) FROM economy");
+                    var totalUserAmount = query[0]["COUNT(*)"];
+
+                    var replyContent = replyContent + `\n\nEnd of the leaderboard. :eyes:\nThis server has **${totalUserAmount}** user${totalUserAmount !== 1 ? 's' : ''} with a balance in the bank.`;
+
+                    if (actualUserAmount == 0) {
+                        var embedReply = embedReplyFailureColor(
+                            "Empty leaderboard.",
+                            "There are no users with a balance on this server.\nUse the `/work` command to be the first one on this list! :grin:",
+                            interaction
+                        );
+                    }
+                    else {
+                        var embedReply = embedReplyPrimaryColor(
+                            `Server leaderboard: TOP ${actualUserAmount} user${actualUserAmount !== 1 ? 's' : ''}.`,
+                            replyContent,
+                            interaction
+                        );
+                    }
+                    break;
+                default:
+                    var embedReply = embedReplyFailureColor(
+                        "Leaderboard - Error",
+                        "Invalid leaderboard type provided as command option parameter.\nPlease choose either `cash` or `bank`.",
+                        interaction
+                    );
             }
         }
 
