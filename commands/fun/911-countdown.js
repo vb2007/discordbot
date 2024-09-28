6f2db93e477015507e2dca5740579f6b2d0a1358
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-09-29 00:55:53 +0200

    added utc offset options

--- commands/fun/911-countdown.js
+++ commands/fun/911-countdown.js
@@ -6,6 +6,52 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("911-countdown")
         .setDescription("Displays how much time is left until 9/11.")
+        .addStringOption(option =>
+            option
+                .setName("utc-offset")
+                .setDescription("The offset that the bot will calculate the time difference with.")
+                .addChoices(
+                    //according to wikipedia: https://en.wikipedia.org/wiki/List_of_UTC_offsets
+                    { name: "UTC-12", value: "-12" },
+                    { name: "UTC-11", value: "-11" },
+                    { name: "UTC-10", value: "-10" },
+                    { name: "UTC-9", value: "-9" },
+                    { name: "UTC-9:30", value: "-930" },
+                    { name: "UTC-8", value: "-8" },
+                    { name: "UTC-7", value: "-7" },
+                    { name: "UTC-6", value: "-6" },
+                    { name: "UTC-5", value: "-5" },
+                    { name: "UTC-4", value: "-4" },
+                    { name: "UTC-3", value: "-3" },
+                    { name: "UTC-3:30", value: "-330" },
+                    { name: "UTC-2", value: "-2" },
+                    { name: "UTC-1", value: "-1" },
+                    { name: "UTC+0", value: "0" },
+                    { name: "UTC+1", value: "1" },
+                    { name: "UTC+2", value: "2" },
+                    { name: "UTC+3", value: "3" },
+                    { name: "UTC+3:30", value: "330" },
+                    { name: "UTC+4", value: "4" },
+                    { name: "UTC+4:30", value: "430" },
+                    { name: "UTC+5", value: "5" },
+                    { name: "UTC+5:30", value: "530" },
+                    { name: "UTC+5:45", value: "545" },
+                    { name: "UTC+6", value: "6" },
+                    { name: "UTC+6:30", value: "630" },
+                    { name: "UTC+7", value: "7" },
+                    { name: "UTC+8", value: "8" },
+                    { name: "UTC+8:45", value: "845" },
+                    { name: "UTC+9", value: "9" },
+                    { name: "UTC+9:30", value: "930" },
+                    { name: "UTC+10", value: "10" },
+                    { name: "UTC+10:30", value: "1030" },
+                    { name: "UTC+11", value: "11" },
+                    { name: "UTC+12", value: "12" },
+                    { name: "UTC+12:45", value: "1245" },
+                    { name: "UTC+13", value: "13" },
+                    { name: "UTC+14", value: "14" }
+                )
+        )
         .setDMPermission(true),
     async execute(interaction) {
         const now = new Date();
