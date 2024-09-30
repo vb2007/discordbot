55b17f4cf24ef53e6784ada71a34bce169429d44
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-09-30 14:37:04 +0200

    updated the /help schema checking test

--- unit-tests/help-commands.test.js
+++ unit-tests/help-commands.test.js
@@ -2,26 +2,21 @@ const fs = require("fs");
 const path = require("path");
 
 describe("/help command test", () => {
-    let helpFileContent;
-    let commandCategories;
 
-    beforeAll(() => {
-        //reads the help.js's content
-        const helpFilePath = path.join(_dirname, "..", "commands", "utility", "help.js");
-        helpFileContent = fs.readFileSync(helpFilePath, "utf-8");
+    const helpFilePath = path.join(__dirname, "..", "commands", "utility", "help.js");
+    const helpFileContent = fs.readFileSync(helpFilePath, "utf-8");
 
-        commandCategories = {
-            utility: "utilityCommands",
-            fun: "funCommands",
-            economy: "economyCommands",
-            moderation: "moderationCommands",
-            administration: "administrationCommands"
-        }
-    });
+    const commandCategories = {
+        utility: "utilityCommands",
+        fun: "funCommands",
+        economy: "economyCommands",
+        moderation: "moderationCommands",
+        administration: "administrationCommands"
+    }
 
     Object.keys(commandCategories).forEach(category => {
         test("All commmand in the filesystem should be listed in the /help command's relevant objects.", () => {
-            const commandsPath = path.join(_dirname, "..", "commands", category);
+            const commandsPath = path.join(__dirname, "..", "commands", category);
             const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
 
             commandFiles.forEach(file => {
