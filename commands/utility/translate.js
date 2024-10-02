d9e2f0e65581fcf3a516dd6adfb72f4f46f9bc5b
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-10-02 14:02:44 +0200

    added slah command documentation and options for /translate

---
+++ commands/utility/translate.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { embedReplyPrimaryColor, embedReplyFailureColor } = require("../../helpers/embed-reply");
+const db = require("../../helpers/db");
+const { logToFileAndDatabase } = require("../../helpers/logger");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("translate")
+        .setDescription("Translates a message to a specified language.")
+        .addStringOption(option =>
+            option
+                .setName("message")
+                .setDescription("The text you would like to translate.")
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName("target-language")
+                .setDescription("The language you would like to translate the message to (default: English).")
+                .setRequired(false)
+        )
+        .addStringOption(option =>
+            option
+                .setName("source-language")
+                .setDescription("The language you would like to translate the message from (default: auto detect).")
+                .setRequired(false)
+        )
+        .setDMPermission(true),
+    async execute(interaction) {
+        
+    }
+}
