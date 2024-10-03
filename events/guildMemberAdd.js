665c3ef249293716835665861074911dcc7346e8
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-10-03 19:14:47 +0200

    updated guildMemberAdd event

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,6 +5,7 @@ module.exports = {
     async execute(member) {
         const guildId = member.guild.id;
 
+        //autorole
         try {
             const rows = await db.query("SELECT roleId FROM autorole WHERE guildId = ?", [guildId]);
             const roleId = rows[0].roleId;
@@ -18,7 +19,25 @@ module.exports = {
             }
         }
         catch (error) {
-            console.error(error);
+            console.error(`Failed to addign role: ${error}`);
+        }
+
+        //welcome messages
+        try {
+            const rows = await db.query("SELECT channelId, message FROM welcome WHERE guildId = ?", [guildId]);
+            const channelId = rows[0].channelId;
+            const message = rows[0].message;
+
+            if (channelId && message) {
+                const channel = member.guild.channels.cache.get(channelId);
+                if (channel) {
+                    await channel.send(message.replace("{user}", member.user.tag));
+                    console.log(`Sent welcome message to ${member.user.tag} in ${member.guild.name}`);
+                }
+            }
+        }
+        catch (error) {
+            console.error(`Failed to display welcome message: ${error}`);
         }
     },
 };
--- sql/welcome/table.sql
+++ sql/welcome/table.sql
@@ -2,7 +2,8 @@
 
 CREATE TABLE IF NOT EXISTS `welcome` (
     `guildId` varchar(50) NOT NULL,
-    `welcomeMessage` text DEFAULT NULL,
+    `channelId` text DEFAULT NULL,
+    `message` text DEFAULT NULL,
     `addedAt` timestamp NOT NULL DEFAULT current_timestamp(),
     `adderId` text DEFAULT NULL,
     `adderUsername` text DEFAULT NULL,
