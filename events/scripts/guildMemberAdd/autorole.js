bf707164d2562cb6e3a936e5b090b088c2d8f394
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-10-05 22:02:43 +0200

    defined member variables at the top of the file

--- events/scripts/guildMemberAdd/autorole.js
+++ events/scripts/guildMemberAdd/autorole.js
@@ -3,6 +3,9 @@ const db = require("../../../helpers/db");
 module.exports = {
     async assignRole (member) {
         const guildId = member.guild.id;
+        const serverName = member.guild.name;
+
+        const userTag = member.user.tag;
 
         try {
             const rows = await db.query("SELECT roleId FROM autorole WHERE guildId = ?", [guildId]);
@@ -12,7 +15,7 @@ module.exports = {
                 const role = member.guild.roles.cache.get(roleId);
                 if (role) {
                     await member.roles.add(role);
-                    console.log(`Assigned ${role.name} role to ${member.user.tag} in ${member.guild.name}`);
+                    console.log(`Assigned ${role.name} role to ${userTag} in ${serverName}`);
                 }
             }
         }
