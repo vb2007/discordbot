7c74fe2ecc7a6aa04869e8b8b5942433292695e3
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-10-05 17:52:13 +0200

    made welcome-configure actually do something

--- commands/administration/welcome-configure.js
+++ commands/administration/welcome-configure.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
-const { embedReplyPrimaryColor, embedReplyFailureColor } = require("../../helpers/embed-reply");
+const { embedReplySuccessColor, embedReplySuccessSecondaryColor, embedReplyWarningColor, embedReplyFailureColor } = require("../../helpers/embed-reply");
 const { logToFileAndDatabase } = require("../../helpers/logger");
 const db = require("../../helpers/db");
 
@@ -28,6 +28,70 @@ module.exports = {
                 "You can only set a welcome message in a server.",
                 interaction
             );
+        }
+        else if (!interaction.guild.members.me.permissions.has(PermissionFlagsBits.Administrator)) {
+            var embedReply = embedReplyFailureColor(
+                "Welcome Disable: Error",
+                "This feature requires **administrator** *(8)* privileges which the bot currently lacks.\nIf you want this feature to work, please re-invite the bot with accurate privileges.",
+                interaction
+            );
+        }
+        else {
+            try {
+                const channelId = interaction.options.get("channel");
+                const welcomeMessage = interaction.options.getString("message");
+                const guildId = interaction.guild.id;
+                const adderId = interaction.user.id;
+                const adderUsername = interaction.user.username;
+
+                const query = await db.query("SELECT guildId, channelId, message FROM welcome WHERE guildId = ?", [guildId]);
+                const existingChannelId = query[0]?.channelId || null;
+                const existingWelcomeMessage = query[0]?.message || null;
+                const existingGuildId = query[0]?.guildId || null;
+
+                //if welcome messages has already been configured for this server...
+                if (welcomeMessage == existingWelcomeMessage && channelId == existingChannelId) {
+                    var embedReply = embedReplyWarningColor(
+                        "Welcome Configure: Warning",
+                        "The same welcome message has already been configured in this server for that channel. :x:\nRun the command with another channel and/or new welcome message to overwrite the current setup.\nRun `/welcome-disable` if you want to disable this feature.",
+                        interaction
+                    );
+                }
+                else {
+                    if (channelId == existingChannelId) {
+                        var embedReply = embedReplySuccessSecondaryColor(
+                            "Welcome Configure: Configuration Modified",
+                            "The **welcome message** has been successfully **modified**. :white_check_mark:\nRun this command again later if you want to modify the message / channel.\nRun `/welcome-disable` if you want to disable this feature.",
+                            interaction
+                        );
+                    }
+                    else if (welcomeMessage == existingWelcomeMessage) {
+                        var embedReply = embedReplySuccessSecondaryColor(
+                            "Welcome Configure: Configuration Modified",
+                            `The **welcome channel** has been successfully **modified** to <#${channelId}>. :white_check_mark:\nRun this command again later if you want to modify the message / channel.\nRun \`/welcome-disable\` if you want to disable this feature.`,
+                            interaction
+                        );
+                    }
+                    else {
+                        var embedReply = embedReplySuccessColor(
+                            "Welcome Configure: Configuration Set",
+                            "The **welcome message** has been successfully **set**. :white_check_mark:\nRun this command again later if you want to modify the message / channel.\nRun `/welcome-disable` if you want to disable this feature.",
+                            interaction
+                        );
+                    }
+
+                    await db.query("INSERT INTO welcome (guildId, channelId, message, adderId, adderUsername) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE channelId = ?, message = ?, adderId = ?, adderUsername = ?",
+                        [guildId, channelId, welcomeMessage, adderId, adderUsername, channelId, welcomeMessage, adderId, adderUsername]
+                    );
+                }
+            }
+            catch (error) {
+                var embedReply = embedReplyFailureColor(
+                    "Welcome Configure: Error",
+                    "There was an error while trying to configure the welcome messages.",
+                    interaction
+                );
+            }
         }
 
         await interaction.reply({ embeds: [embedReply] });
