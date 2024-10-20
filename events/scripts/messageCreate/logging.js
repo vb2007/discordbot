e0c0dbc75abd15bede1c48a9a149abf92f097e81
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-10-20 23:55:53 +0200

    made logging to local db configureable

--- events/scripts/messageCreate/logging.js
+++ events/scripts/messageCreate/logging.js
@@ -1,25 +1,28 @@
 const db = require("../../../helpers/db");
+const { logMessagesToLocalDatabase } = require("../../../config.json");
 
 module.exports = {
     async logMessage(message) {
-        const messageContent = message.content;
-        const senderUserName = message.author.username;
-        const senderUserId = message.author.id;
-        const serverName = message.guild.name;
-        const serverId = message.guild.id;
-        const channelName = message.channel.name;
-        const channelId = message.channel.id;
+        if (logMessagesToLocalDatabase == "True") {
+            const messageContent = message.content;
+            const senderUserName = message.author.username;
+            const senderUserId = message.author.id;
+            const serverName = message.guild.name;
+            const serverId = message.guild.id;
+            const channelName = message.channel.name;
+            const channelId = message.channel.id;
 
-        try {
-            await db.query("INSERT INTO messageLog (messageContent, senderUserName, senderUserId, serverName, serverId, channelName, channelId) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                [
-                    messageContent, senderUserName, senderUserId, serverName, serverId, channelName, channelId
-                ]
-            );
-            console.log(`Logged message "${message.content}" from ${senderUserName} in ${serverName}.`);
-        }
-        catch (error) {
-            console.error(`Failed to log message: ${error}`);
+            try {
+                await db.query("INSERT INTO messageLog (messageContent, senderUserName, senderUserId, serverName, serverId, channelName, channelId) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                    [
+                        messageContent, senderUserName, senderUserId, serverName, serverId, channelName, channelId
+                    ]
+                );
+                console.log(`Logged message "${message.content}" from ${senderUserName} in ${serverName}.`);
+            }
+            catch (error) {
+                console.error(`Failed to log message: ${error}`);
+            }
         }
     }
 }
--- index.js
+++ index.js
@@ -60,7 +60,6 @@ const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
 	const event = require(filePath);
-	console.log("Event: ", event);
 
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
