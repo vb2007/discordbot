94c34f1e4212cff0a2026d339584812824f1128a
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-10-24 00:38:01 +0200

    added emojiDelete event logging

---
+++ events/scripts/emojiDelete/logEventToChannel.js
@@ -0,0 +1,28 @@
+const db = require("../../../helpers/db");
+const { embedMessageFailureColor } = require("../../../helpers/embeds/embed-message");
+
+module.exports = {
+    async sendLogInfo(emoji) {
+        const guildId = emoji.guild.id;
+
+        try {
+            const query = await db.query("SELECT guildId, logChannelId FROM configLogging WHERE guildId = ?", [guildId]);
+            const existingGuildId = query[0]?.guildId;
+
+            if (existingGuildId) {
+                const logChannelId = query[0]?.logChannelId;
+                const logChannel = emoji.guild.channels.cache.get(logChannelId);
+
+                const logEmbed = embedMessageFailureColor(
+                    "Emoji deleted",
+                    `${emoji.name} emoji was deleted.`,
+                );
+
+                await logChannel.send({ embeds: [logEmbed] });
+            }
+        }
+        catch (error) {
+            console.error(`Failed to send log info to target channel: ${error}`);
+        }
+    }
+}
