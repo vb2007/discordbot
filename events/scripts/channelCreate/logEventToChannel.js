8d0d4a9894f6043961e2c91492695736207700ac
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-10-28 21:50:33 +0100

    added channel type helper, updated channel creation logging

--- events/scripts/channelCreate/logEventToChannel.js
+++ events/scripts/channelCreate/logEventToChannel.js
@@ -1,5 +1,6 @@
 const { getGuildFromDB } = require("../../../helpers/log-data-query");
-const { embedMessageSuccessColor } = require("../../../helpers/embeds/embed-message");
+const { getChannelType } = require("../../../helpers/channel-types");
+const { embedMessageSuccessColorWithFields } = require("../../../helpers/embeds/embed-message");
 
 module.exports = {
     async sendLogInfo(channel) {
@@ -9,9 +10,16 @@ module.exports = {
             if (doesGuildExist) {
                 const logChannel = channel.guild.channels.cache.get(logChannelId);
 
-                const logEmbed = embedMessageSuccessColor(
+                const logEmbed = embedMessageSuccessColorWithFields(
                     "Channel created",
-                    `${channel.name} channel was created.`,
+                    `Channel '<#${channel.id}>' was created.`,
+                    [
+                        { name: "Name", value: `${channel.name}`, inline: true },
+                        { name: "Id", value: `${channel.id}`, inline: true },
+                        { name: "Category", value: `${channel.parent.name}` },
+                        { name: "Type", value: `${getChannelType(channel)}`, inline: true },
+                        { name: "NSFW?", value: `${channel.nsfw ? "Yes" : "No"}`, inline: true },
+                    ]
                 );
 
                 await logChannel.send({ embeds: [logEmbed] });
