df3ce40e833309b867957bbad4fb7309c0a99833
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-10-30 18:11:14 +0100

    updated /bridge-configure with relevant checks and db updates

--- commands/administration/bridge-configure.js
+++ commands/administration/bridge-configure.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
-const { embedReplySuccessColor, embedReplyFailureColor } = require("../../helpers/embeds/embed-reply");
+const { embedReplySuccessColor, embedReplyFailureColor, embedReplySuccessSecondaryColor } = require("../../helpers/embeds/embed-reply");
 const { logToFileAndDatabase } = require("../../helpers/logger");
 const db = require("../../helpers/db");
 
@@ -33,31 +33,48 @@ module.exports = {
         else {
             try {
                 const sourceChannelId = interaction.options.getString("source-channel-id");
-                const targetChannel = interaction.options.getChannel("destination-channel");
+                const destinationChannel = interaction.options.getChannel("destination-channel");
 
                 const guildId = interaction.guild.id;
                 const guildName = interaction.guild.name;
-                const targetChannelId = targetChannel.id;
-                const targetChannelName = targetChannel.name;
+                const destinationChannelId = destinationChannel.id;
+                const destinationChannelName = destinationChannel.name;
                 const interactionUserId = interaction.user.id;
                 const interactionUsername = interaction.user.username;
 
-                const query = await db.query("SELECT destinationChannelId, sourceChannelId, destinationGuildId FROM configBridging WHERE sourceChannelId = ? AND destinationChannelId = ?", [sourceChannelId, targetChannelId]);
+                const query = await db.query("SELECT destinationChannelId, sourceChannelId, destinationGuildId FROM configBridging WHERE sourceChannelId = ? AND destinationChannelId = ?", [sourceChannelId, destinationChannelId]);
                 const destinationGuildId = query[0]?.destinationGuildId || null;
                 const existingSourceChannelId = query[0]?.sourceChannelId || null;
                 const existingDestinationChannelId = query[0]?.destinationChannelId || null;
 
-                if (existingSourceChannelId == sourceChannelId && existingDestinationChannelId == targetChannelId) {
+                if (existingSourceChannelId == sourceChannelId && existingDestinationChannelId == destinationChannelId && destinationGuildId == guildId) {
                     var embedReply = embedReplyFailureColor(
                         "Bridge Configure: Error",
                         `Bridging has already been configured for the channel <#${sourceChannelId}> (\`${sourceChannelId}\`). :x:\n`,
                         interaction
                     );
                 }
-                else {
+                else if (existingSourceChannelId == sourceChannelId && destinationGuildId == guildId) {
+                    var embedReply = embedReplySuccessSecondaryColor(
+                        "Bridge Configure: Configuration Modified",
+                        `The destination channel for <#${sourceChannelId}> (\`${sourceChannelId}\`) has been updated to <#${destinationChannelId}>. :white_check_mark:\nRun this command again to modify the channel.`,
+                    );
+
+                    await db.query("UPDATE configBridging SET destinationChannelId = ?, destinationChannelName = ?, adderId = ?, adderUsername = ? WHERE sourceChannelId = ? AND destinationChannelId = ?",
+                        [
+                            destinationChannelId,
+                            destinationChannelName,
+                            interactionUserId,
+                            interactionUsername,
+                            sourceChannelId,
+                            destinationChannelId
+                        ]
+                    );
+                }
+                else if (existingSourceChannelId != sourceChannelId && destinationGuildId == guildId) {
                     var embedReply = embedReplySuccessColor(
                         "Bridge Configure: Success",
-                        `Bridging has been successfully configured. :white_check_mark:\nMessages from <#${sourceChannelId}> (\`${sourceChannelId}\`) will now get bridged to <#${targetChannelId}>.`,
+                        `Another channel has been added to bridging successfully. :white_check_mark:\nMessages from <#${sourceChannelId}> (\`${sourceChannelId}\`) will now get bridged to <#${destinationChannelId}>.`,
                     );
 
                     await db.query("INSERT INTO configBridging (sourceChannelId, destinationGuildId, destinationGuildName, destinationChannelId, destinationChannelName, adderId, adderUsername) VALUES (?, ?, ?, ?, ?, ?, ?)",
@@ -65,8 +82,26 @@ module.exports = {
                             sourceChannelId,
                             guildId,
                             guildName,
-                            targetChannelId,
-                            targetChannelName,
+                            destinationChannelId,
+                            destinationChannelName,
+                            interactionUserId,
+                            interactionUsername
+                        ]
+                    );
+                }
+                else {
+                    var embedReply = embedReplySuccessColor(
+                        "Bridge Configure: Success",
+                        `Bridging has been successfully configured. :white_check_mark:\nMessages from <#${sourceChannelId}> (\`${sourceChannelId}\`) will now get bridged to <#${destinationChannelId}>.`,
+                    );
+
+                    await db.query("INSERT INTO configBridging (sourceChannelId, destinationGuildId, destinationGuildName, destinationChannelId, destinationChannelName, adderId, adderUsername) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                        [
+                            sourceChannelId,
+                            guildId,
+                            guildName,
+                            destinationChannelId,
+                            destinationChannelName,
                             interactionUserId,
                             interactionUsername
                         ]
