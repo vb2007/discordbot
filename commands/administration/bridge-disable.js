262e9c88ca92064115e8ef2d79be557e349abb85
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-10-31 10:19:39 +0100

    added base concept for /bridge-disable

--- commands/administration/bridge-disable.js
+++ commands/administration/bridge-disable.js
@@ -16,6 +16,46 @@ module.exports = {
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
         .setDMPermission(false),
     async execute(interaction) {
-        
+        if (!interaction.inGuild()) {
+            var embedReply = embedReplyFailureColor(
+                "Bridge Disable: Error",
+                "You can only disable bridging in a server.",
+                interaction
+            );
+        }
+        else {
+            try {
+                const sourceChannelId = interaction.options.getString("source-channel-id");
+                const guildId = interaction.guild.id;
+
+                const query = await db.query("SELECT sourceChannelId FROM configBridging WHERE sourceChannelId = ? AND destinationGuildId = ?", [sourceChannelId, guildId]);
+                const existingGuildId = query[0]?.guildId || null;
+                const existingSourceChannelId = query[0]?.sourceChannelId || null;
+
+                if (existingSourceChannelId && existingGuildId) {
+                    await db.query("DELETE FROM configBridging WHERE sourceChannelId = ? AND destinationGuildId = ?", [sourceChannelId, guildId]);
+                    var embedReply = embedReplySuccessColor(
+                        "Bridge Disable: Success",
+                        `Bridging has been disabled for the channel <#${sourceChannelId}> (\`${sourceChannelId}\`). :white_check_mark:\nYou can re-enable this feature with \`/bridge-configure\`.`,
+                        interaction
+                    );
+                }
+                else {
+                    var embedReply = embedReplyFailureColor(
+                        "Bridge Disable: Error",
+                        `Bridging has not been configured for the channel <#${sourceChannelId}> (\`${sourceChannelId}\`). :x:\nTherefore, you can't disable it.\nYou can enable this feature with \`/bridge-configure\`.`,
+                        interaction
+                    );
+                }
+            }
+            catch (error) {
+                console.error(`Error while disabling bridging: ${error}`);
+                var embedReply = embedReplyFailureColor(
+                    "Bridge Disable: Error",
+                    "An error occurred while disabling the bridging feature.\nPlease try again.",
+                    interaction
+                );
+            }
+        }
     }
 }
