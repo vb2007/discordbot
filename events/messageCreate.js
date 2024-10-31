2c47e688681a776605860162bb7c74a1eae8d81a
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-10-31 12:00:23 +0100

    added script to messageCreate event

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,8 +1,10 @@
 const messageLogging = require("./scripts/messageCreate/messageLogging");
+const bridge = require("./scripts/messageCreate/bridge");
 
 module.exports = {
     name: "messageCreate",
     async execute(message) {
         await messageLogging.logMessagesToLocalDatabase(message);
+        await bridge.sendMessageToDestinationChannel(message);
     },
 };
