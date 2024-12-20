203ff7a3cde595c27ca497bd67fd7e26dffbcc8b
VB2007 <admin@vb2007.hu>
2024-12-20 23:48:10 +0100

    moved command descriptions in /help to github wiki due to discord's message legth limitations

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -24,58 +24,58 @@ module.exports = {
 
         const utilityCommands = 
         { name: "Utility", value:
-            "`/help` - Displays this message.\n" +
-            "`/ping` - Displays the discord API's current latency.\n" +
-            "`/ping-db` - Displays the current latency between the bot and it's database.\n" +
-            "`/server` - Provides information about the current server.\n" +
-            "`/user` - Provides information about a specified user.\n" +
-            "`/translate` - Translates a message from any language to any language.\n" +
-            "`/say` - Makes the bot say a specified message."
+            "`/help`\n" +
+            "`/ping`\n" +
+            "`/ping-db`\n" +
+            "`/server`\n" +
+            "`/user`\n" +
+            "`/translate`\n" +
+            "`/say`"
         };
 
         const funCommands = 
         { name: "Fun", value:
-            "`/coinflip` - Flips a coin that has a 50/50 chance landing on heads or tails.\n" +
-            "`/randompic` - Send a random picture using the [picsum.photos](https://picsum.photos/) API.\n" +
-            "`/randomfeet` - I have nothing to say about my greatest shame...\n" +
-            "`/911-countdown` - Displays huw much time is left until Spetember 11th."
+            "`/coinflip`\n" +
+            "`/randompic`\n" +
+            "`/randomfeet`\n" +
+            "`/911-countdown`"
         };
 
         const economyCommands = 
         { name: "Economy", value:
-            "`/work` - Lets you work for a random amount of money.\n" +
-            "`/beg` - Lets you beg for a random (or no) amount of money.\n" +
-            "`/rob` - Steals a random amount of money from the target user, and adds it to your balance.\n" +
-            "`/balance` - Displays the user's current bank and handheld balance.\n" +
-            "`/deposit` - Deposits money into the user's bank account. It has a daily limit for free deposits, but the users can choose to pay a small deposit fee if they exceed the limit.\n" +
-            "`/withdraw` - Withdraws money from the user's bank account.\n" +
-            "`/pay` - Transfers money from one user to another.\n" +
-            "`/roulette` - Lets you pick a color, then gives you a great price if you guess the color right.\n" +
-            "`/leaderboard` - Displays users with the most money on the server."
+            "`/work`\n" +
+            "`/beg`\n" +
+            "`/rob`\n" +
+            "`/balance`\n" +
+            "`/deposit`\n" +
+            "`/withdraw`\n" +
+            "`/pay`\n" +
+            "`/roulette`\n" +
+            "`/leaderboard`"
         };
 
         const moderationCommands = 
         { name: "Moderation", value:
             "**__NOTE__**: The following commands require relevant **moderation permissions** for both the bot and the command's executor to work.\n" +
-            "`/warn` - Warns a specified member on the server.\n" +
-            "`/timeout` - Times out a specified member for a specified time.\n" +
-            "`/kick` - Kicks a specified member from the server.\n" +
-            "`/ban` - Bans a specified member from the server.\n" +
-            "`/purge` - Purges (mass deletes) a specified amount of messages from the current channel."
+            "`/warn`\n" +
+            "`/timeout`\n" +
+            "`/kick`\n" +
+            "`/ban`\n" +
+            "`/purge`"
         };
 
         const administrationCommands = 
         { name: "Administration", value:
             "**__NOTE__**: The following commands require relevant **administration permissions** for both the bot and the command's executor to work.\n" +
-            "`/autorole-configure` - Sets / modifies the autorole feature. When a new member joins the server, a specified role will get assigned to them automatically.\n" +
-            "`/autorole-disable` - Disables the autorole feature. New members won't get the specified role automatically on join anymore.\n" +
-            "`/welcome-configure` - Sets / modifies the welcome messages feature. When a new member joins the server, the bot send a specified welcome message.\n" +
-            "`/welcome-disable` - Disables the welcome messages feature. The bot won't send a welcome message on join anymore.\n" +
-            "`/logging-configure` - Sets / modifies the channel where event on the server will get logged.\n" +
-            "`/logging-disable` - Disables the logging feature. The bot won't log the events on the server anymore.\n" +
-            "`/bridge-configure` - Bridges all messages from one channel to another.\n" +
-            "`/bridge-disable` - Disables briding for a target channel.\n" +
-            "`/rename` - Renames a specified user to a specified nickname in the current server."
+            "`/autorole-configure`\n" +
+            "`/autorole-disable`\n" +
+            "`/welcome-configure`\n" +
+            "`/welcome-disable`\n" +
+            "`/logging-configure`\n" +
+            "`/logging-disable`\n" +
+            "`/bridge-configure`\n" +
+            "`/bridge-disable`\n" +
+            "`/rename`"
         };
 
         const tipField = 
