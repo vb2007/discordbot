67cfa0af7595f6e5ce6210f1874aee8c8b42b48f
VB2007 <admin@vb2007.hu>
2024-12-22 00:17:06 +0100

    made /help work with specified commands

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -21,29 +21,31 @@ module.exports = {
                 .setRequired(false))
         .addStringOption(option =>
             option
-                .setName("commandName")
+                .setName("command")
                 .setDescription("Specify a command's name you would like to know more information about.")
                 .setRequired(false))
         .setDMPermission(true),
     async execute(interaction) {
         const commandCategory = interaction.options.getString("category");
-        const commandName = interaction.options.getString("commandName");
+        const commandName = interaction.options.getString("command");
         
-        if (commandCategory && command) {
+        if (commandCategory && commandName) {
             var embedReply = embedReplyFailureColor(
                 "Help: Error",
                 "Please only provide one parameter for the command.",
                 interaction
             );
         }
-        if (!commandCategory) {
+        if (!commandName) {
             const commandsQuery = await db.query("SELECT name, category FROM commandData WHERE category = ?", [commandCategory]);
             console.log(commandsQuery);
         }
-        else if (!command) {
+        else if (!commandCategory) {
             const commandQuery = await db.query("SELECT category, description FROM commandData WHERE name = ?", [commandName]);
             const category = commandQuery[0]?.category || "Unknown";
             const description = commandQuery[0]?.description || "Unknown";
+            console.log(category);
+            console.log(description);
 
             var embedReply = embedReplyPrimaryColorWithFields(
                 `Help: /${commandName}`,
@@ -56,7 +58,7 @@ module.exports = {
             );
         }
         else {
-
+            console.log();
         }
 
         const tipField = 
@@ -70,66 +72,65 @@ module.exports = {
             "Please choose a valid category: `utility`, `fun`, `economy`, `moderation`, `administration`."
         }
 
-        let embedReply;
-        if (!commandCategory) {
-            embedReply = embedReplyPrimaryColorWithFields(
-                "Help - All Commands.",
-                "Here is a list of the bot's currently avaliable commands:",
-                [ utilityCommands, funCommands, economyCommands, moderationCommands, administrationCommands, tipField ],
-                interaction
-            );
-        }
-        else {
-            switch (commandCategory) {
-                case "utility":
-                    embedReply = embedReplyPrimaryColorWithFields(
-                        "Help - Utility Commands.",
-                        "Here is a list of the bot's currently avaliable **utility** commands:",
-                        [ utilityCommands ],
-                        interaction
-                    );
-                    break;
-                case "fun":
-                    embedReply = embedReplyPrimaryColorWithFields(
-                        "Help - Fun Commands.",
-                        "Here is a list of the bot's currently avaliable **fun** commands:",
-                        [ funCommands ],
-                        interaction
-                    );
-                    break;
-                case "economy":
-                    embedReply = embedReplyPrimaryColorWithFields(
-                        "Help - Economy Commands.",
-                        "Here is a list of the bot's currently avaliable **economy** commands:",
-                        [ economyCommands ],
-                        interaction
-                    );
-                    break;
-                case "moderation":
-                    embedReply = embedReplyPrimaryColorWithFields(
-                        "Help - Moderation Commands.",
-                        "Here is a list of the bot's currently avaliable **moderation** commands:",
-                        [ moderationCommands ],
-                        interaction
-                    );
-                    break;
-                case "administration":
-                    embedReply = embedReplyPrimaryColorWithFields(
-                        "Help - Administration Commands.",
-                        "Here is a list of the bot's currently avaliable **administration** commands:",
-                        [ administrationCommands ],
-                        interaction
-                    );
-                    break;
-                default:
-                    embedReply = embedReplyErrorColorWithFields(
-                        "Help - Utility Commands.",
-                        "Here is a list of the bot's currently avaliable **utility** commands:",
-                        [ errorField ],
-                        interaction
-                    );
-            }
-        }
+        // if (!commandCategory) {
+        //     embedReply = embedReplyPrimaryColorWithFields(
+        //         "Help - All Commands.",
+        //         "Here is a list of the bot's currently avaliable commands:",
+        //         [ utilityCommands, funCommands, economyCommands, moderationCommands, administrationCommands, tipField ],
+        //         interaction
+        //     );
+        // }
+        // else {
+        //     switch (commandCategory) {
+        //         case "utility":
+        //             embedReply = embedReplyPrimaryColorWithFields(
+        //                 "Help - Utility Commands.",
+        //                 "Here is a list of the bot's currently avaliable **utility** commands:",
+        //                 [ utilityCommands ],
+        //                 interaction
+        //             );
+        //             break;
+        //         case "fun":
+        //             embedReply = embedReplyPrimaryColorWithFields(
+        //                 "Help - Fun Commands.",
+        //                 "Here is a list of the bot's currently avaliable **fun** commands:",
+        //                 [ funCommands ],
+        //                 interaction
+        //             );
+        //             break;
+        //         case "economy":
+        //             embedReply = embedReplyPrimaryColorWithFields(
+        //                 "Help - Economy Commands.",
+        //                 "Here is a list of the bot's currently avaliable **economy** commands:",
+        //                 [ economyCommands ],
+        //                 interaction
+        //             );
+        //             break;
+        //         case "moderation":
+        //             embedReply = embedReplyPrimaryColorWithFields(
+        //                 "Help - Moderation Commands.",
+        //                 "Here is a list of the bot's currently avaliable **moderation** commands:",
+        //                 [ moderationCommands ],
+        //                 interaction
+        //             );
+        //             break;
+        //         case "administration":
+        //             embedReply = embedReplyPrimaryColorWithFields(
+        //                 "Help - Administration Commands.",
+        //                 "Here is a list of the bot's currently avaliable **administration** commands:",
+        //                 [ administrationCommands ],
+        //                 interaction
+        //             );
+        //             break;
+        //         default:
+        //             embedReply = embedReplyErrorColorWithFields(
+        //                 "Help - Utility Commands.",
+        //                 "Here is a list of the bot's currently avaliable **utility** commands:",
+        //                 [ errorField ],
+        //                 interaction
+        //             );
+        //     }
+        // }
 
         await interaction.reply({ embeds: [embedReply] });
 
