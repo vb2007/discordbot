8f681b06b891ebe9202b9c9151a5281169e0119a
VB2007 <admin@vb2007.hu>
2025-06-24 23:06:27 +0200

    added sql script for darwin's cache

---
+++ src/sql/darwinCache/table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `darwinCache` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `videoUrl` VARCHAR(255) NOT NULL,
+    `processedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE KEY (`videoUrl`)
+);
