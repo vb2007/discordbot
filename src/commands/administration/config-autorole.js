ab407ae5c30b21aac269bb2ea744321a5a46dd8b
VB2007 <admin@vb2007.hu>
2025-09-04 22:04:16 +0200

    added action handling to autorole config

--- src/commands/administration/config-autorole.js
+++ src/commands/administration/config-autorole.js
@@ -25,61 +25,65 @@ module.exports = {
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles)
         .setDMPermission(false),
     async execute(interaction) {
-        if (!interaction.inGuild()) {
-            var embedReply = embedReplyFailureColor(
-                "AutoRole Configure: Error",
-                "You can only set up autorole in a server.",
-                interaction
-            );
-        }
-        else if (!interaction.guild.members.me.permissions.has(PermissionFlagsBits.Administrator)) {
-            var embedReply = embedReplyFailureColor(
-                "AutoRole Configure: Error",
-                "This feature requires **administrator** *(8)* privileges witch the bot currently lacks.\nIf you want this feature to work, please re-invite the bot with accurate privileges.",
-                interaction
-            );
-        }
-        else {
-            try {
-                const targetRole = interaction.options.get("role").value;
-                const adderUsername = interaction.user.username;
-                const adderId = interaction.user.id;
-                const guildId = interaction.guild.id;
+        const action = interaction.options.getString("action");
 
-                const query = await db.query("SELECT guildId, roleId FROM configAutorole WHERE guildId = ?", [guildId]);
-                const autoRoleGuildId = query[0]?.guildId || null;
-                const autoRoleRoleId = query[0]?.roleId || null;
+        if (action === "configure") {
+            if (!interaction.inGuild()) {
+                var embedReply = embedReplyFailureColor(
+                    "AutoRole Configure: Error",
+                    "You can only set up autorole in a server.",
+                    interaction
+                );
+            }
+            else if (!interaction.guild.members.me.permissions.has(PermissionFlagsBits.Administrator)) {
+                var embedReply = embedReplyFailureColor(
+                    "AutoRole Configure: Error",
+                    "This feature requires **administrator** *(8)* privileges witch the bot currently lacks.\nIf you want this feature to work, please re-invite the bot with accurate privileges.",
+                    interaction
+                );
+            }
+            else {
+                try {
+                    const targetRole = interaction.options.get("role").value;
+                    const adderUsername = interaction.user.username;
+                    const adderId = interaction.user.id;
+                    const guildId = interaction.guild.id;
 
-                //if autorole has already been configured for this server...
-                if (autoRoleRoleId == targetRole) {
-                    var embedReply = embedReplyFailureColor(
-                        "AutoRole Configure: Error",
-                        "Autorole has already been configured for this server with this role. :x:\nRun the command with another role to overwrite the current role.\nRun `/autorole-disable` to disable this feature completely.",
-                        interaction
-                    );
-                }
-                else {
-                    if (autoRoleGuildId == guildId) {
-                        //if the target role is already the role that's in the database, then we don't need to insert data
-                        var embedReply = embedReplySuccessSecondaryColor(
-                            "AutoRole Configure: Configuration Modified",
-                            `The role that will get assigned to new members has been **modified** to <@&${targetRole}>. :white_check_mark:\nRun this command again to modify the role.\nRun \`/autorole-disable\` to disable this feature.`,
+                    const query = await db.query("SELECT guildId, roleId FROM configAutorole WHERE guildId = ?", [guildId]);
+                    const autoRoleGuildId = query[0]?.guildId || null;
+                    const autoRoleRoleId = query[0]?.roleId || null;
+
+                    //if autorole has already been configured for this server...
+                    if (autoRoleRoleId == targetRole) {
+                        var embedReply = embedReplyFailureColor(
+                            "AutoRole Configure: Error",
+                            "Autorole has already been configured for this server with this role. :x:\nRun the command with another role to overwrite the current role.\nRun `/autorole-disable` to disable this feature completely.",
                             interaction
                         );
                     }
                     else {
-                        var embedReply = embedReplySuccessColor(
-                            "AutoRole Configure: Configuration Set",
-                            `The role that will get assigned to new members has been **set** to <@&${targetRole}>. :white_check_mark:\nRun this command again to modify the role.\nRun \`/autorole-disable\` to disable this feature.`,
-                            interaction
-                        );
-                    }
+                        if (autoRoleGuildId == guildId) {
+                            //if the target role is already the role that's in the database, then we don't need to insert data
+                            var embedReply = embedReplySuccessSecondaryColor(
+                                "AutoRole Configure: Configuration Modified",
+                                `The role that will get assigned to new members has been **modified** to <@&${targetRole}>. :white_check_mark:\nRun this command again to modify the role.\nRun \`/autorole-disable\` to disable this feature.`,
+                                interaction
+                            );
+                        }
+                        else {
+                            var embedReply = embedReplySuccessColor(
+                                "AutoRole Configure: Configuration Set",
+                                `The role that will get assigned to new members has been **set** to <@&${targetRole}>. :white_check_mark:\nRun this command again to modify the role.\nRun \`/autorole-disable\` to disable this feature.`,
+                                interaction
+                            );
+                        }
 
-                    await db.query("INSERT INTO configAutorole (guildId, roleId, adderId, adderUsername) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE roleId = ?, adderId = ?, adderUsername = ?", [guildId, targetRole, adderId, adderUsername, targetRole, adderId, adderUsername]);
+                        await db.query("INSERT INTO configAutorole (guildId, roleId, adderId, adderUsername) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE roleId = ?, adderId = ?, adderUsername = ?", [guildId, targetRole, adderId, adderUsername, targetRole, adderId, adderUsername]);
+                    }
                 }
-            }
-            catch (error) {
-                console.error(error);
+                catch (error) {
+                    console.error(error);
+                }
             }
         }
 
