265a0eb204e7e7a633a4286cbe3c3ae465d6ebe8
VB2007 <admin@vb2007.hu>
2025-09-14 22:18:47 +0200

    fixed missing import in helper, added disable logic to /config-darwin

--- src/helpers/command-validation/general.js
+++ src/helpers/command-validation/general.js
@@ -1,32 +1,37 @@
+const { PermissionFlagsBits } = require("discord.js");
 const { embedReplyFailureColor } = require("../embeds/embed-reply");
 const { capitalizeFirstLetter } = require("../format");
 
 function checkIfNotInGuild(commandName, interaction) {
-    if (!interaction.inGuild()) {
-        const embedReply = embedReplyFailureColor(
-            `${capitalizeFirstLetter(commandName)} - Not in a server`,
-            `You can only use the \`/${commandName}\` command in a server.`,
-            interaction
-        );
+  if (!interaction.inGuild()) {
+    const embedReply = embedReplyFailureColor(
+      `${capitalizeFirstLetter(commandName)} - Not in a server`,
+      `You can only use the \`/${commandName}\` command in a server.`,
+      interaction,
+    );
 
-        return embedReply;
-    }
+    return embedReply;
+  }
 
-    return null;
+  return null;
 }
 
 function checkAdminPermissions(commandName, interaction) {
-    if (!interaction.guild.members.me.permissions.has(PermissionFlagsBits.Administrator)) {
-        const embedReply = embedReplyFailureColor(
-            `${capitalizeFirstLetter(commandName)} - Insufficient Permissions`,
-            `This feature requires **administrator** *(8)* privileges which the bot currently lacks.\nIf you want this feature to work, please re-invite the bot with accurate privileges.`,
-            interaction
-        );
+  if (
+    !interaction.guild.members.me.permissions.has(
+      PermissionFlagsBits.Administrator,
+    )
+  ) {
+    const embedReply = embedReplyFailureColor(
+      `${capitalizeFirstLetter(commandName)} - Insufficient Permissions`,
+      `This feature requires **administrator** *(8)* privileges which the bot currently lacks.\nIf you want this feature to work, please re-invite the bot with accurate privileges.`,
+      interaction,
+    );
 
-        return embedReply;
-    }
+    return embedReply;
+  }
 
-    return null;
+  return null;
 }
 
-module.exports = { checkIfNotInGuild, checkAdminPermissions }
+module.exports = { checkIfNotInGuild, checkAdminPermissions };
