bde9387d59778dea555b68009f6d9033e8ce74ef
VB2007 <admin@vb2007.hu>
2025-09-15 09:12:02 +0200

    updated config-goodbye.js

--- src/commands/administration/config-goodbye.js
+++ src/commands/administration/config-goodbye.js
@@ -7,6 +7,7 @@ const {
 } = require("../../helpers/embeds/embed-reply");
 const {
   checkIfNotInGuild,
+  checkAdminPermissions,
 } = require("../../helpers/command-validation/general");
 const replyAndLog = require("../../helpers/reply");
 const db = require("../../helpers/db");
@@ -67,125 +68,117 @@ module.exports = {
 
     const action = interaction.options.getString("action");
 
-    if (!interaction.inGuild()) {
-      var embedReply = embedReplyFailureColor(
-        "Goodbye Configure: Error",
-        "You can only set a goodbye message in a server.",
-        interaction,
-      );
-    } else if (
-      !interaction.guild.members.me.permissions.has(
-        PermissionFlagsBits.Administrator,
-      )
-    ) {
-      var embedReply = embedReplyFailureColor(
-        "Goodbye Configure: Error",
-        "This feature requires **administrator** *(8)* privileges which the bot currently lacks.\nIf you want this feature to work, please re-invite the bot with accurate privileges.",
-        interaction,
-      );
-    } else {
-      try {
-        const channelId = interaction.options.getChannel("channel").id;
-        const goodbyeMessage = interaction.options.getString("message");
-        const isEmbed = interaction.options.getBoolean("embed") || 0;
-        const embedColor =
-          interaction.options.getInteger("embed-color") || null;
-
-        const guildId = interaction.guild.id;
-        const adderId = interaction.user.id;
-        const adderUsername = interaction.user.username;
-
-        const query = await db.query(
-          "SELECT channelId, message, isEmbed, embedColor FROM configGoodbye WHERE guildId = ?",
-          [guildId],
-        );
-        const existingChannelId = query[0]?.channelId || null;
-        const existingGoodbyeMessage = query[0]?.message || null;
-        const existingIsEmbed = query[0]?.isEmbed || 0;
-        const existingEmbedColor = query[0]?.embedColor || null;
-
-        //if goodbye messages haven't been configured for the current server
-        if (!existingChannelId) {
-          var embedReply = embedReplySuccessColor(
-            "Goodbye Configure: Configuration Set",
-            "The **goodbye message** has been successfully **set**. :white_check_mark:\nRun this command again later if you want to modify the current configuration.\nRun `/goodbye-disable` if you want to disable this feature.",
-            interaction,
-          );
-        } else {
-          //checks if anything has been modified in the the command
-          let modifications = [];
-          if (goodbyeMessage != existingGoodbyeMessage) {
-            modifications.push("**goodbye message**");
-          }
-          if (isEmbed != existingIsEmbed) {
-            modifications.push("**embed option**");
-          }
-          if (embedColor != existingEmbedColor) {
-            modifications.push("**embed color**");
-          }
-          if (channelId != existingChannelId) {
-            modifications.push(`**goodbye channel** to <#${channelId}>`);
-          }
-
-          //if the exact same goodbye configuration is set for the current server (aka. nothing got modified)
-          if (modifications.length === 0) {
-            var embedReply = embedReplyWarningColor(
-              "Goodbye Configure: Warning",
-              "The exact same goodbye configuration has been set for this server already. :x:\nRun the command again with different options to overwrite the current configuration.\nRun `/goodbye-disable`, if you want to disable this feature.",
-              interaction,
-            );
-          }
-          //if the goodbye configuration has been modified
-          else {
-            let modificationsMessage;
-            if (modifications.length === 1) {
-              modificationsMessage = modifications[0];
-            } else {
-              modificationsMessage =
-                modifications.slice(0, -1).join(", ") +
-                " and " +
-                modifications[modifications.length - 1];
-            }
-
-            var embedReply = embedReplySuccessSecondaryColor(
-              "Goodbye Configure: Configuration Modified",
-              `Successfully modified ${modificationsMessage}. :white_check_mark:\nRun the command again with different options to overwrite the current configuration.\nRun \`/goodbye-disable\`, if you want to disable this feature.`,
-              interaction,
-            );
-          }
-        }
-
-        await db.query(
-          "INSERT INTO configGoodbye (guildId, channelId, message, isEmbed, embedColor, adderId, adderUsername) VALUES (?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE channelId = ?, message = ?, adderId = ?, adderUsername = ?, isEmbed = ?, embedColor = ?",
-          [
-            guildId,
-            channelId,
-            goodbyeMessage,
-            isEmbed,
-            embedColor,
-            adderId,
-            adderUsername,
-            channelId,
-            goodbyeMessage,
-            adderId,
-            adderUsername,
-            isEmbed,
-            embedColor,
-          ],
-        );
-      } catch (error) {
-        var embedReply = embedReplyFailureColor(
-          "Goodbye Configure: Error",
-          "There was an error while trying to configure the goodbye messages.",
-          interaction,
-        );
-      }
+    const guildCheck = checkIfNotInGuild(commandName, interaction);
+    if (guildCheck) {
+      return await replyAndLog(interaction, guildCheck);
     }
 
-    await interaction.reply({ embeds: [embedReply] });
-    await logToFileAndDatabase(
-      interaction,
-      JSON.stringify(embedReply.toJSON()),
-    );
+    const permissionCheck = checkAdminPermissions(commandName, interaction);
+    if (permissionCheck) {
+      return await replyAndLog(interaction, permissionCheck);
+    }
+
+    try {
+      const channelId = interaction.options.getChannel("channel").id;
+      const goodbyeMessage = interaction.options.getString("message");
+      const isEmbed = interaction.options.getBoolean("embed") || 0;
+      const embedColor = interaction.options.getInteger("embed-color") || null;
+
+      const guildId = interaction.guild.id;
+      const adderId = interaction.user.id;
+      const adderUsername = interaction.user.username;
+
+      const query = await db.query(
+        "SELECT channelId, message, isEmbed, embedColor FROM configGoodbye WHERE guildId = ?",
+        [guildId],
+      );
+      const existingChannelId = query[0]?.channelId || null;
+      const existingGoodbyeMessage = query[0]?.message || null;
+      const existingIsEmbed = query[0]?.isEmbed || 0;
+      const existingEmbedColor = query[0]?.embedColor || null;
+
+      await db.query(
+        "INSERT INTO configGoodbye (guildId, channelId, message, isEmbed, embedColor, adderId, adderUsername) VALUES (?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE channelId = ?, message = ?, adderId = ?, adderUsername = ?, isEmbed = ?, embedColor = ?",
+        [
+          guildId,
+          channelId,
+          goodbyeMessage,
+          isEmbed,
+          embedColor,
+          adderId,
+          adderUsername,
+          channelId,
+          goodbyeMessage,
+          adderId,
+          adderUsername,
+          isEmbed,
+          embedColor,
+        ],
+      );
+
+      //if goodbye messages haven't been configured for the current server
+      if (!existingChannelId) {
+        title = "Goodbye Configure: Configuration Set";
+        description =
+          "The **goodbye message** has been successfully **set**. :white_check_mark:\nRun this command again later if you want to modify or disable the current configuration.";
+        return replyAndLog(
+          interaction,
+          embedReplySuccessColor(title, description, interaction),
+        );
+      }
+
+      //checks if anything has been modified in the the command
+      let modifications = [];
+      if (goodbyeMessage != existingGoodbyeMessage) {
+        modifications.push("**goodbye message**");
+      }
+      if (isEmbed != existingIsEmbed) {
+        modifications.push("**embed option**");
+      }
+      if (embedColor != existingEmbedColor) {
+        modifications.push("**embed color**");
+      }
+      if (channelId != existingChannelId) {
+        modifications.push(`**goodbye channel** to <#${channelId}>`);
+      }
+
+      //if the exact same goodbye configuration is set for the current server (aka. nothing got modified)
+      if (modifications.length === 0) {
+        title = "Goodbye Configure: Warning";
+        description =
+          "The exact same goodbye configuration has been set for this server already. :x:\nRun the command again with different options to overwrite or disable the current configuration.";
+        return replyAndLog(
+          interaction,
+          embedReplyWarningColor(title, description, interaction),
+        );
+      }
+
+      let modificationsMessage;
+      if (modifications.length === 1) {
+        modificationsMessage = modifications[0];
+      } else {
+        modificationsMessage =
+          modifications.slice(0, -1).join(", ") +
+          " and " +
+          modifications[modifications.length - 1];
+      }
+
+      title = "Goodbye Configure: Configuration Modified";
+      description = `Successfully modified ${modificationsMessage}. :white_check_mark:\nRun the command again with different options to overwrite or disable the current configuration.`;
+      return replyAndLog(
+        interaction,
+        embedReplySuccessSecondaryColor(title, description, interaction),
+      );
+    } catch (error) {
+      console.error(`Error while running ${commandName}: ${error}`);
+
+      title = "Goodbye Configure: Error";
+      description =
+        "There was an error while trying to configure the goodbye messages.\nPlease try again later.";
+      return replyAndLog(
+        interaction,
+        embedReplyFailureColor(title, description, interaction),
+      );
+    }
   },
 };
