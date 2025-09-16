09962a86a5e354cc99ea531652ceb907f61600e5
VB2007 <admin@vb2007.hu>
2025-09-16 18:02:16 +0200

    rewritten config-welcome.js using helpers

--- src/commands/administration/config-welcome.js
+++ src/commands/administration/config-welcome.js
@@ -10,6 +10,7 @@ const {
   checkAdminPermissions,
 } = require("../../helpers/command-validation/general");
 const db = require("../../helpers/db");
+const replyAndLog = require("../../helpers/reply");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -62,23 +63,22 @@ module.exports = {
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
     .setDMPermission(false),
   async execute(interaction) {
-    if (!interaction.inGuild()) {
-      var embedReply = embedReplyFailureColor(
-        "Welcome Configure: Error",
-        "You can only set a welcome message in a server.",
-        interaction,
-      );
-    } else if (
-      !interaction.guild.members.me.permissions.has(
-        PermissionFlagsBits.Administrator,
-      )
-    ) {
-      var embedReply = embedReplyFailureColor(
-        "Welcome Configure: Error",
-        "This feature requires **administrator** *(8)* privileges which the bot currently lacks.\nIf you want this feature to work, please re-invite the bot with accurate privileges.",
-        interaction,
-      );
-    } else {
+    let title;
+    let description;
+
+    const action = interaction.options.getString("action");
+
+    const guildCheck = checkIfNotInGuild(commandName, interaction);
+    if (guildCheck) {
+      return await replyAndLog(interaction, guildCheck);
+    }
+
+    const permissionCheck = checkAdminPermissions(commandName, interaction);
+    if (permissionCheck) {
+      return await replyAndLog(interaction, permissionCheck);
+    }
+
+    if (action === "confiugre") {
       try {
         const channelId = interaction.options.getChannel("channel").id;
         const welcomeMessage = interaction.options.getString("message");
@@ -101,54 +101,58 @@ module.exports = {
 
         //if welcome messages haven't been configured for the current server
         if (!existingChannelId) {
-          var embedReply = embedReplySuccessColor(
-            "Welcome Configure: Configuration Set",
-            "The **welcome message** has been successfully **set**. :white_check_mark:\nRun this command again later if you want to modify the current configuration.\nRun `/welcome-disable` if you want to disable this feature.",
+          title = "Welcome Configure: Configuration Set";
+          description =
+            "The **welcome message** has been successfully **set**. :white_check_mark:\nRun this command again later if you want to modify or disable the current configuration.";
+          return replyAndLog(
             interaction,
+            embedReplySuccessColor(title, description, interaction),
           );
-        } else {
-          //checks if anything has been modified in the the command
-          let modifications = [];
-          if (welcomeMessage != existingWelcomeMessage) {
-            modifications.push("**welcome message**");
-          }
-          if (isEmbed != existingIsEmbed) {
-            modifications.push("**embed option**");
-          }
-          if (embedColor != existingEmbedColor) {
-            modifications.push("**embed color**");
-          }
-          if (channelId != existingChannelId) {
-            modifications.push(`**welcome channel** to <#${channelId}>`);
-          }
-
-          //if the exact same welcome configuration is set for the current server (aka. nothing got modified)
-          if (modifications.length === 0) {
-            var embedReply = embedReplyWarningColor(
-              "Welcome Configure: Warning",
-              "The exact same welcome configuration has been set for this server already. :x:\nRun the command again with different options to overwrite the current configuration.\nRun `/welcome-disable`, if you want to disable this feature.",
-              interaction,
-            );
-          }
-          //if the welcome configuration has been modified
-          else {
-            let modificationsMessage;
-            if (modifications.length === 1) {
-              modificationsMessage = modifications[0];
-            } else {
-              modificationsMessage =
-                modifications.slice(0, -1).join(", ") +
-                " and " +
-                modifications[modifications.length - 1];
-            }
-
-            var embedReply = embedReplySuccessSecondaryColor(
-              "Welcome Configure: Configuration Modified",
-              `Successfully modified ${modificationsMessage}. :white_check_mark:\nRun the command again with different options to overwrite the current configuration.\nRun \`/welcome-disable\`, if you want to disable this feature.`,
-              interaction,
-            );
-          }
         }
+
+        //checks if anything has been modified in the the command
+        let modifications = [];
+        if (welcomeMessage != existingWelcomeMessage) {
+          modifications.push("**welcome message**");
+        }
+        if (isEmbed != existingIsEmbed) {
+          modifications.push("**embed option**");
+        }
+        if (embedColor != existingEmbedColor) {
+          modifications.push("**embed color**");
+        }
+        if (channelId != existingChannelId) {
+          modifications.push(`**welcome channel** to <#${channelId}>`);
+        }
+
+        //if the exact same welcome configuration is set for the current server (aka. nothing got modified)
+        if (modifications.length === 0) {
+          title = "Welcome Configure: Warning";
+          description =
+            "The exact same welcome configuration has been set for this server already. :x:\nRun the command again with different options to overwrite or disable the current configuration";
+          return replyAndLog(
+            interaction,
+            embedReplyWarningColor(title, description, interaction),
+          );
+        }
+
+        //if the welcome configuration has been modified
+        let modificationsMessage;
+        if (modifications.length === 1) {
+          modificationsMessage = modifications[0];
+        } else {
+          modificationsMessage =
+            modifications.slice(0, -1).join(", ") +
+            " and " +
+            modifications[modifications.length - 1];
+        }
+
+        title = "Welcome Configure: Configuration Modified";
+        description = `Successfully modified ${modificationsMessage}. :white_check_mark:\nRun the command again with different options to overwrite or disable the current configuration.`;
+        return replyAndLog(
+          interaction,
+          embedReplySuccessSecondaryColor(title, description, interaction),
+        );
 
         await db.query(
           "INSERT INTO configWelcome (guildId, channelId, message, isEmbed, embedColor, adderId, adderUsername) VALUES (?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE channelId = ?, message = ?, adderId = ?, adderUsername = ?, isEmbed = ?, embedColor = ?",
@@ -169,18 +173,16 @@ module.exports = {
           ],
         );
       } catch (error) {
-        var embedReply = embedReplyFailureColor(
-          "Welcome Configure: Error",
-          "There was an error while trying to configure the welcome messages.",
+        console.error(`Error while running ${commandName}: ${error}`);
+
+        title = "Welcome Configure: Error";
+        description =
+          "There was an error while trying to configure the welcome messages.";
+        return replyAndLog(
           interaction,
+          embedReplyFailureColor(title, description, interaction),
         );
       }
     }
-
-    await interaction.reply({ embeds: [embedReply] });
-    await logToFileAndDatabase(
-      interaction,
-      JSON.stringify(embedReply.toJSON()),
-    );
   },
 };
