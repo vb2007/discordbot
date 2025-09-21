17f4d54170dce12cf5d584f44b9343ad0173b7d2
VB2007 <admin@vb2007.hu>
2025-09-21 18:34:40 +0200

    updated helper commands formatting

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 const databaseHostAddress = process.env.DATABASE_HOST_ADDRESS;
 const databaseName = process.env.DATABASE_NAME;
 const databaseUser = process.env.DATABASE_USER;
@@ -9,49 +9,50 @@ const mariadb = require("mariadb");
 let pool;
 
 function getConnection() {
-    return new Promise((resolve, reject) => {
-        if (pool) {
+  return new Promise((resolve, reject) => {
+    if (pool) {
+      resolve(pool);
+    } else {
+      try {
+        pool = mariadb.createPool({
+          host: databaseHostAddress,
+          database: databaseName,
+          user: databaseUser,
+          password: databasePassword,
+          connectionLimit: 5,
+        });
+
+        pool
+          .getConnection()
+          .then((conn) => {
+            console.log(
+              `Database connection successful to host '${databaseHostAddress}', database '${databaseName}' with user '${databaseUser}'.`,
+            );
+            conn.release();
             resolve(pool);
-        }
-        else {
-            try {
-                pool = mariadb.createPool({
-                    host: databaseHostAddress,
-                    database: databaseName,
-                    user: databaseUser,
-                    password: databasePassword,
-                    connectionLimit: 5
-                });
-            
-                pool.getConnection()
-                    .then(conn => {
-                        console.log(`Database connection successful to host '${databaseHostAddress}', database '${databaseName}' with user '${databaseUser}'.`);
-                        conn.release();
-                        resolve(pool);
-                    })
-                    .catch(reject);
-            }
-            catch (error) {
-                reject(error);
-            }
-        }
-    });
+          })
+          .catch(reject);
+      } catch (error) {
+        reject(error);
+      }
+    }
+  });
 }
 
 function handleError(error) {
-    console.error("Error while trying to connect to the database: ", error);
-    process.exit(1);
+  console.error("Error while trying to connect to the database: ", error);
+  process.exit(1);
 }
 
 function query(...args) {
-    if (!pool) {
-        handleError(new Error("Database pool isn't initialized."));
-    }
-    return pool.query(...args).catch(handleError);
+  if (!pool) {
+    handleError(new Error("Database pool isn't initialized."));
+  }
+  return pool.query(...args).catch(handleError);
 }
 
 module.exports = {
-    getConnection,
-    query,
-    end: () => pool && pool.end()
+  getConnection,
+  query,
+  end: () => pool && pool.end(),
 };
