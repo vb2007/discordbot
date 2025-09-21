509154d0cb6c133ff30b02e47a41493f89a0aac8
VB2007 <admin@vb2007.hu>
2025-09-21 19:18:55 +0200

    implemented image scraping logic in /cigany

--- src/commands/fun/cigany.js
+++ src/commands/fun/cigany.js
@@ -1,29 +1,49 @@
 const { SlashCommandBuilder } = require("discord.js");
 const {
-  embedReplyPrimaryColor,
+  embedReplyPrimaryColorImg,
   embedReplyFailureColor,
 } = require("../../helpers/embeds/embed-reply");
 const replyAndLog = require("../../helpers/reply");
+const { logToFileAndDatabase } = require("../../helpers/logger");
 
 const commandName = "cigany";
 
 module.exports = {
-  data: new SlashCommandBuilder().setName(commandName).setDescription("fuj"),
+  data: new SlashCommandBuilder()
+    .setName(commandName)
+    .setDescription("fuj")
+    .setDMPermission(true),
   async execute(interaction) {
+    await interaction.deferReply();
+
     let title;
     let description;
+
+    const links = await loadLinks(
+      "cigpics.json",
+      "https://cdn.vb2007.hu/autoindex/kardoscigok/",
+    );
+    const randomCig = links[Math.floor(Math.random() * links.length)];
 
     try {
       title = "Cigány";
       description = "Fuj";
 
-      return await replyAndLog(
+      var embedReply = embedReplyPrimaryColorImg(
+        title,
+        description,
+        randomCig,
         interaction,
-        embedReplyPrimaryColor(title, description, interaction),
+      );
+
+      await interaction.editReply({ embeds: [embedReply] });
+      return await logToFileAndDatabase(
+        interaction,
+        JSON.stringify(embedReply.toJSON()),
       );
     } catch (error) {
       title = "Error";
-      description = "Meg ciganyozni se tud a bot. :(";
+      description = "Meg cigányozni se tud a bot. :(";
 
       return await replyAndLog(
         interaction,
