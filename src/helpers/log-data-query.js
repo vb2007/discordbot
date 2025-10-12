d779933321dcf5b93f24a34d6ce2bc1ba9fcf144
VB2007 <admin@vb2007.hu>
2025-10-12 15:58:06 +0200

    updated import / export in log-data-query

--- src/helpers/log-data-query.js
+++ src/helpers/log-data-query.js
@@ -1,26 +1,24 @@
-const db = require("./db");
+import { query } from "./db.js";
 
-module.exports = {
-  /**
-   * @param {object} object - An channel, message, etc. object from a guild
-   * @returns {doesGuildExist} A boolean value indicating if the guild exists in the database
-   * @returns {logChannelId} A log channel ID from the database based on the guild ID (if configured)
-   */
-  async getGuildFromDB(guildId) {
-    try {
-      if (!guildId) return { doesGuildExist: false, logChannelId: null };
+/**
+ * @param {object} object - An channel, message, etc. object from a guild
+ * @returns {doesGuildExist} A boolean value indicating if the guild exists in the database
+ * @returns {logChannelId} A log channel ID from the database based on the guild ID (if configured)
+ */
+export const getGuildFromDB = async (guildId) => {
+  try {
+    if (!guildId) return { doesGuildExist: false, logChannelId: null };
 
-      const query = await db.query(
-        "SELECT guildId, logChannelId FROM configLogging WHERE guildId = ?",
-        [guildId]
-      );
-      const logChannelId = query[0]?.logChannelId;
+    const result = await query(
+      "SELECT guildId, logChannelId FROM configLogging WHERE guildId = ?",
+      [guildId]
+    );
+    const logChannelId = result[0]?.logChannelId;
 
-      const doesGuildExist = logChannelId ? true : false;
-      return { doesGuildExist, logChannelId };
-    } catch (error) {
-      console.error(`Failed to get logging config data from the database: ${error}`);
-      return { doesGuildExist, logChannelId };
-    }
-  },
+    const doesGuildExist = logChannelId ? true : false;
+    return { doesGuildExist, logChannelId };
+  } catch (error) {
+    console.error(`Failed to get logging config data from the database: ${error}`);
+    return { doesGuildExist, logChannelId };
+  }
 };
