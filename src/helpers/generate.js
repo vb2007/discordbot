65949b850125e4366ea89d2949ee2c07a456ac92
VB2007 <admin@vb2007.hu>
2025-10-12 15:58:48 +0200

    updated export in generate.js

--- src/helpers/generate.js
+++ src/helpers/generate.js
@@ -1,13 +1,9 @@
 //Function(s) used by the /roulette command
-function generateRandomOutcome() {
+export const generateRandomOutcome = () => {
   const rouletteNumbers = Array.from({ length: 37 }, (_, index) => index); //0 to 36 for European roulette
   const randomIndex = Math.floor(Math.random() * rouletteNumbers.length);
   const number = rouletteNumbers[randomIndex];
   //green if number is 0, black if it's divisible by 2, red otherwise
   const color = number === 0 ? "green" : number % 2 === 0 ? "black" : "red";
   return { number, color };
-}
-
-module.exports = {
-  generateRandomOutcome,
 };
