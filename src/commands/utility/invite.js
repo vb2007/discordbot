be2de9b8c31e889af07af5b4c3eddb7c34a98e7b
VB2007 <admin@vb2007.hu>
2025-10-19 15:11:53 +0200

    created base for /invite

---
+++ src/commands/utility/invite.js
@@ -0,0 +1,16 @@
+import { SlashCommandBuilder } from "discord.js";
+import { embedReplyPrimaryColor } from "../../helpers/embeds/embed-reply.js";
+import { replyAndLog } from "../../helpers/reply.js";
+
+import "dotenv/config";
+const clientId = process.env.CLIENT_ID;
+
+const commandName = "invite";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName(commandName)
+    .setDescription("Gives you an URL to invite the bot to any server.")
+    .setDMPermission(true),
+  async execute(interaction) {},
+};
