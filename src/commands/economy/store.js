7041917f6ade5fea0be0156986f6e74281fd677a
VB2007 <admin@vb2007.hu>
2025-10-30 23:59:10 +0100

    created base for command

---
+++ src/commands/economy/store.js
@@ -0,0 +1,7 @@
+import { SlashCommandBuilder } from "discord.js";
+import { embedReplyPrimaryColorWithFields } from "../../helpers/embeds/embed-reply.js";
+
+export default {
+  data: new SlashCommandBuilder(),
+  async execute(interaction) {},
+};
