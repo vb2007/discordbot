d65820923adc64ed42fdbead665c3550b87398a2
VB2007 <admin@vb2007.hu>
2025-11-13 20:48:09 +0100

    renamed, added columns to darwinCache table

--- src/sql/darwinCache/table.sql
+++ src/sql/darwinCache/table.sql
@@ -2,8 +2,12 @@
 
 CREATE TABLE IF NOT EXISTS `darwinCache` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `videoUrl` VARCHAR(255) NOT NULL,
+    `originalFileUrl` VARCHAR(255) NOT NULL,
+    `forumPostUrl` VARCHAR(255) NOT NULL,
     `videoId` VARCHAR(100) NOT NULL,
+    `videoTitle` VARCHAR(180) NOT NULL,
+    `originalFileSize` DECIMAL NOT NULL,
+    `processedFileSize` DECIMAL DEFAULT NULL,
     `processedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     UNIQUE KEY (`videoUrl`),
     UNIQUE KEY (`videoId`)
