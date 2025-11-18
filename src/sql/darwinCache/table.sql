b0ba7fe1c02df4f887071a76c20f3698c3c2001c
VB2007 <admin@vb2007.hu>
2025-11-19 00:10:01 +0100

    updated darwinCache table schema, added migration script

--- src/sql/darwinCache/table.sql
+++ src/sql/darwinCache/table.sql
@@ -2,9 +2,11 @@
 
 CREATE TABLE IF NOT EXISTS `darwinCache` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `videoUrl` VARCHAR(255) NOT NULL,
+    `directVideoUrl` VARCHAR(255) NOT NULL,
+    `forumPostUrl` VARCHAR(255) NOT NULL,
     `videoId` VARCHAR(100) NOT NULL,
+    `videoTitle` VARCHAR(180) NOT NULL,
     `processedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE KEY (`videoUrl`),
+    UNIQUE KEY (`directVideoUrl`),
     UNIQUE KEY (`videoId`)
 );
