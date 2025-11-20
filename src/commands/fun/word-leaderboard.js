618dc45e8350676a671495a1180987eed7d8771b
VB2007 <admin@vb2007.hu>
2025-11-20 19:21:00 +0100

    created word-leaderboard.js command file

---
+++ src/commands/fun/word-leaderboard.js
@@ -0,0 +1,7 @@
+import { SlashCommandBuilder } from "discord.js";
+import { query } from "../../helpers/db.js";
+
+export default {
+  data: new SlashCommandBuilder(),
+  async execute(interaction) {},
+};
