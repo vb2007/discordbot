cc9009e068ce9275e37cf5974d61d2ca136c14a7
VB2007 <admin@vb2007.hu>
2025-11-21 03:19:30 +0100

    updated column name in sql file

--- src/sql/darwinCache/table.sql
+++ src/sql/darwinCache/table.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS `darwinCache` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `originalFileUrl` VARCHAR(255) NOT NULL,
+    `directVideoUrl` VARCHAR(255) NOT NULL,
     `forumPostUrl` VARCHAR(255) NOT NULL,
     `videoId` VARCHAR(100) NOT NULL,
     `videoTitle` VARCHAR(180) NOT NULL,
