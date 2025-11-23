39ee60747fd4eb68772d0bc0109feb9153c77441
VB2007 <admin@vb2007.hu>
2025-11-23 01:35:33 +0100

    added execution logic for /store

--- src/commands/economy/store.js
+++ src/commands/economy/store.js
@@ -1,7 +1,24 @@
 import { SlashCommandBuilder } from "discord.js";
 import { embedReplyPrimaryColorWithFields } from "../../helpers/embeds/embed-reply.js";
+import { checkIfNotInGuild } from "../../helpers/command-validation/general.js";
+import { query } from "../../helpers/db.js";
+
+const commandName = "store";
 
 export default {
-  data: new SlashCommandBuilder(),
-  async execute(interaction) {},
+  data: new SlashCommandBuilder()
+    .setName(commandName)
+    .setDescription("")
+    .setNSFW(false)
+    .setDMPermission(false),
+  async execute(interaction) {
+    const guildCheck = checkIfNotInGuild(commandName, interaction);
+    if (guildCheck) {
+      return await replyAndLog(interaction, guildCheck);
+    }
+
+    const embedReply = embedReplyPrimaryColorWithFields("Store", "", {}, interaction);
+
+    return await replyAndLog(interaction, embedReply);
+  },
 };
