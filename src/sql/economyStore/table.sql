4b1d24cf5f83fa693538f9e130114929ed362807
VB2007 <admin@vb2007.hu>
2025-11-30 00:21:56 +0100

    implemented economyStore sql table definition

---
+++ src/sql/economyStore/table.sql
@@ -0,0 +1,9 @@
+-- discordbot.economyStore definition
+
+CREATE TABLE IF NOT EXISTS `economyStore` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `price` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(100) DEFAULT 'No description provided.',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
