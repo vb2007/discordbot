15560551764e1a491281c865313ffd52f1aeaa75
VB2007 <admin@vb2007.hu>
2026-01-25 17:35:39 +0100

    created economyTable sql script

---
+++ src/sql/economyInventory/table.sql
@@ -0,0 +1,6 @@
+-- discordbotTest.economyInventory definition
+
+CREATE TABLE IF NOT EXISTS `economyInventory` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
