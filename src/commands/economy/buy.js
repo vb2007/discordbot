d1b09415b087e09eca227b22eba6fa241edf24a3
VB2007 <admin@vb2007.hu>
2026-01-25 17:33:01 +0100

    updated tab with in .prettierrc

--- src/commands/economy/buy.js
+++ src/commands/economy/buy.js
@@ -1,9 +1,9 @@
 import { SlashCommandBuilder } from "discord.js";
 import {
-  embedReplySuccessColor,
-  embedReplyFailureColor,
-  embedReply,
-  embedReplyWarningColor,
+    embedReplySuccessColor,
+    embedReplyFailureColor,
+    embedReply,
+    embedReplyWarningColor,
 } from "../../helpers/embeds/embed-reply.js";
 import { checkIfNotInGuild } from "../../helpers/command-validation/general.js";
 import { replyAndLog } from "../../helpers/reply.js";
@@ -12,38 +12,38 @@ import { query } from "../../helpers/db.js";
 const commandName = "buy";
 
 export default {
-  data: new SlashCommandBuilder()
-    .setName("buy")
-    .setDescription("Let's you buy a specified item.")
-    .addStringOption((option) =>
-      option
-        .setName("item-name")
-        .setDescription("The item's exact name you would like to buy.")
-        .setMinLength(2)
-        .setMaxLength(10)
-        .setRequired(true)
-    )
-    .setNSFW(false)
-    .setDMPermission(false),
-  async execute(interaction) {
-    const guildCheck = checkIfNotInGuild(commandName, interaction);
-    if (guildCheck) {
-      return await replyAndLog(interaction, guildCheck);
-    }
+    data: new SlashCommandBuilder()
+        .setName("buy")
+        .setDescription("Let's you buy a specified item.")
+        .addStringOption((option) =>
+            option
+                .setName("item-name")
+                .setDescription("The item's exact name you would like to buy.")
+                .setMinLength(2)
+                .setMaxLength(10)
+                .setRequired(true)
+        )
+        .setNSFW(false)
+        .setDMPermission(false),
+    async execute(interaction) {
+        const guildCheck = checkIfNotInGuild(commandName, interaction);
+        if (guildCheck) {
+            return await replyAndLog(interaction, guildCheck);
+        }
 
-    const itemName = interaction.options.getString("item-name");
+        const itemName = interaction.options.getString("item-name");
 
-    const allItemsQuery = await query("SELECT price, name FROM economyStore");
-    const validItem = allItemsQuery.find((item) => item.name === itemName);
+        const allItemsQuery = await query("SELECT price, name FROM economyStore");
+        const validItem = allItemsQuery.find((item) => item.name === itemName);
 
-    if (!validItem) {
-      const embedReply = embedReplyWarningColor(
-        "Buy: Invalid Item",
-        `\`${itemName}\` isn't a valid item.\nUse the \`/store\` command to see all purchaseable items.`,
-        interaction
-      );
+        if (!validItem) {
+            const embedReply = embedReplyWarningColor(
+                "Buy: Invalid Item",
+                `\`${itemName}\` isn't a valid item.\nUse the \`/store\` command to see all purchaseable items.`,
+                interaction
+            );
 
-      return await replyAndLog(interaction, embedReply);
-    }
-  },
+            return await replyAndLog(interaction, embedReply);
+        }
+    },
 };
